f69a5c8a8ff6d4bb66363efe87b746fe83343f6b
matheusrebola <m.cavalcantir@outlook.com>
2024-10-09 14:43:11 -0300

    feat: inclusão do método get para buscar todas as reações

--- local/src/main/java/propagandus/api/core/controller/ReacaoController.java
+++ local/src/main/java/propagandus/api/core/controller/ReacaoController.java
@@ -1,7 +1,11 @@
 package propagandus.api.core.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,5 +33,11 @@ public class ReacaoController {
     ReacaoDto respondeDto = reacaoMapper.map(reacaoSaved);
     return new ResponseEntity<>(respondeDto, HttpStatus.CREATED);
   }
+
+  @GetMapping
+    public ResponseEntity<List<ReacaoDto>> getAll() {
+        List<ReacaoDto> result = reacaoService.getAll().stream().map(reacaoMapper::map).collect(Collectors.toList());
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
 
 }
