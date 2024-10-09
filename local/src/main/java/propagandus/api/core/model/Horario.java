348cda0a59f2830934068f6ff1d7a904ea918737
matheusrebola <m.cavalcantir@outlook.com>
2024-10-09 11:18:01 -0300

    feat: criação das relações entre as entidades e adição de atributos

--- local/src/main/java/propagandus/api/core/model/Horario.java
+++ local/src/main/java/propagandus/api/core/model/Horario.java
@@ -1,17 +1,23 @@
 package propagandus.api.core.model;
 
+import java.util.List;
+
 import org.hibernate.validator.constraints.UUID;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import propagandus.api.core.model.enums.EHorario;
 
 @Entity
 public record Horario(
   @Id @GeneratedValue(strategy = GenerationType.UUID) UUID id,
-  EHorario tipoHorario
+  @Enumerated(EnumType.STRING) EHorario tipoHorario,
+  @OneToMany(mappedBy = "horario") List<Reacao> reacoes
 ) {
 
 }
--- local/src/main/java/propagandus/api/core/model/Reacao.java
+++ local/src/main/java/propagandus/api/core/model/Reacao.java
@@ -5,20 +5,27 @@ import java.util.UUID;
 import java.time.LocalDateTime;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import propagandus.api.core.model.enums.EAtencao;
 import propagandus.api.core.model.enums.EReacao;
 
 @Entity
 public record Reacao(
   @Id @GeneratedValue(strategy = GenerationType.UUID) UUID id,
-  EReacao tipoReacao,
-  Pessoa pessoa,
-  Horario horario,
-  EAtencao nivelAtencao,
-  LocalDateTime dataReacao
+  @Enumerated(EnumType.STRING) EReacao tipoReacao,
+  @ManyToOne @JoinColumn(name = "pessoa_id") Pessoa pessoa,
+  @ManyToOne @JoinColumn(name = "horario_id") Horario horario,
+  @Enumerated(EnumType.STRING) EAtencao nivelAtencao,
+  LocalDateTime dataReacao,
+  @ManyToOne @JoinColumn(name = "local_id") Local localReacao,
+  @ManyToOne @JoinColumn(name = "painel_id") Painel painel,
+  long tempoFixacao
 ) {
 
 }
