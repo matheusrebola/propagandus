acf019dae81a5bcb8cc2fb6736cf50722ecc0fc3
matheusrebola <m.cavalcantir@outlook.com>
2024-10-09 00:39:40 -0300

    feat: criação dos mircroserviços com arquitetura no padrão saga orquestrado

---
+++ microservice/reacao/src/test/java/propagandus/reacao/ReacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package propagandus.reacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ microservice/sender/sender/src/test/java/propagandus/sender/SenderApplicationTests.java
@@ -0,0 +1,13 @@
+package propagandus.sender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
