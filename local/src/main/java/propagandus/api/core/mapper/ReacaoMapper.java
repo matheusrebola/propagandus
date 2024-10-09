78cc9ee64eedda91a11e1f4df5a9f532ea522896
matheusrebola <m.cavalcantir@outlook.com>
2024-10-09 11:59:19 -0300

    feat: remoção da anotação para required args contructor e da constante

--- local/src/main/java/propagandus/api/core/mapper/ReacaoMapper.java
+++ local/src/main/java/propagandus/api/core/mapper/ReacaoMapper.java
@@ -3,15 +3,13 @@ package propagandus.api.core.mapper;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
-import lombok.RequiredArgsConstructor;
 import propagandus.api.core.dto.ReacaoCreateDto;
 import propagandus.api.core.dto.ReacaoDto;
 import propagandus.api.core.model.Reacao;
 
 @Component
-@RequiredArgsConstructor
 public class ReacaoMapper {
-  private final ModelMapper modelMapper;
+  private ModelMapper modelMapper;
 
   public Reacao map(ReacaoCreateDto dto){
     Reacao reacao = modelMapper.map(dto, Reacao.class);
