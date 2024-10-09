2f836911f3c12a63e008c5b96faf21332eb40473
matheusrebola <m.cavalcantir@outlook.com>
2024-10-09 11:12:00 -0300

    feat: criação do endpoint de post para reações vindas da IA

--- local/src/main/java/propagandus/api/core/controller/ReacaoController.java
+++ local/src/main/java/propagandus/api/core/controller/ReacaoController.java
@@ -1,5 +1,33 @@
 package propagandus.api.core.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import propagandus.api.core.dto.ReacaoCreateDto;
+import propagandus.api.core.dto.ReacaoDto;
+import propagandus.api.core.mapper.ReacaoMapper;
+import propagandus.api.core.model.Reacao;
+import propagandus.api.core.service.ReacaoService;
+
+@RestController
+@RequestMapping("/reacoes")
+@RequiredArgsConstructor
 public class ReacaoController {
+  private final ReacaoService reacaoService;
+  private final ReacaoMapper reacaoMapper;
+
+  @PostMapping
+  public ResponseEntity<ReacaoDto> create(@Valid @RequestBody ReacaoCreateDto createDto){
+    Reacao reacao = reacaoMapper.map(createDto);
+    Reacao reacaoSaved = reacaoService.save(reacao);
+    ReacaoDto respondeDto = reacaoMapper.map(reacaoSaved);
+    return new ResponseEntity<>(respondeDto, HttpStatus.CREATED);
+  }
 
 }
