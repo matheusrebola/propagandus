c9db6d7ac60109fd74bc43ba8e961b8c4d6236e1
matheusrebola <matheusrebola@outlook.com>
2024-12-05 20:45:50 -0300

    feat: criação do novo microserviço

---
+++ service/cloud-layer/data-organizer-service/src/main/java/propagandus/dataorganizerservice/DataOrganizerServiceApplication.java
@@ -0,0 +1,13 @@
+package propagandus.dataorganizerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataOrganizerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataOrganizerServiceApplication.class, args);
+	}
+
+}
