7bb5385dc25c1ee5be86dc2af73d89ff1bda35b1
matheusrebola <matheusrebola@outlook.com>
2024-12-05 18:34:32 -0300

    feat: adição da implementação da view

--- service/cloud-layer/analitics-service/src/main/java/propagandus/analiticsservice/core/repositorys/AdvertisingRepository.java
+++ service/cloud-layer/analitics-service/src/main/java/propagandus/analiticsservice/core/repositorys/AdvertisingRepository.java
@@ -6,9 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import propagandus.analiticsservice.core.models.Advertising;
+import propagandus.analiticsservice.core.views.AdvertisingView;
 
 @Repository
-public interface AdvertisingRepository extends JpaRepository<Advertising, Long> {
+public interface AdvertisingRepository extends JpaRepository<Advertising, Long>, AdvertisingView {
   String findByName(String name);
   List<String> findByCompany(String company);
   List<String> findByProduct(String product);
