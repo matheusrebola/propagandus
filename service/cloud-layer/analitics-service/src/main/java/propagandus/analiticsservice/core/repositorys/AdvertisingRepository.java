5a1c5bd3ba6626c82bcc6b1b0657f7a753bf887f
matheusrebola <matheusrebola@outlook.com>
2024-12-05 12:47:21 -0300

    feat: criação de métodos para busca por atributos das entidades

--- service/cloud-layer/analitics-service/src/main/java/propagandus/analiticsservice/core/repositorys/AdvertisingRepository.java
+++ service/cloud-layer/analitics-service/src/main/java/propagandus/analiticsservice/core/repositorys/AdvertisingRepository.java
@@ -1,5 +1,7 @@
 package propagandus.analiticsservice.core.repositorys;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import propagandus.analiticsservice.core.models.Advertising;
 
 @Repository
 public interface AdvertisingRepository extends JpaRepository<Advertising, Long> {
-
+  String findByName(String name);
+  List<String> findByCompany(String company);
+  List<String> findByProduct(String product);
 }
--- service/cloud-layer/analitics-service/src/main/java/propagandus/analiticsservice/core/repositorys/PainelRepository.java
+++ service/cloud-layer/analitics-service/src/main/java/propagandus/analiticsservice/core/repositorys/PainelRepository.java
@@ -1,11 +1,16 @@
 package propagandus.analiticsservice.core.repositorys;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import propagandus.analiticsservice.core.models.Painel;
+import propagandus.analiticsservice.core.models.enumerators.EStatus;
 
 @Repository
 public interface PainelRepository extends JpaRepository<Painel, Long> {
-
+  String findByIdentification(String identification);
+  EStatus findByStatus(EStatus status);
+  List<String> findByModel(String model);
 }
