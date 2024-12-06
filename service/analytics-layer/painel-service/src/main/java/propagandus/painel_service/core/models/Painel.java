002da1040a10f5074f3db9378dfb29bcd8c8062a
matheusrebola <matheusrebola@outlook.com>
2024-12-06 14:57:45 -0300

    feat: remoção de imports desnecessários

--- service/analytics-layer/painel-service/src/main/java/propagandus/painel_service/core/models/Painel.java
+++ service/analytics-layer/painel-service/src/main/java/propagandus/painel_service/core/models/Painel.java
@@ -1,15 +1,5 @@
 package propagandus.painel_service.core.models;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
 import propagandus.painel_service.core.models.enumerators.EStatus;
 
 public record Painel(
