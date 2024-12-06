f954c9786d238bb5469526bf8ff2865b1d6dbfe8
matheusrebola <matheusrebola@outlook.com>
2024-12-06 15:08:28 -0300

    feat: remoção de enums desnecessários

--- service/analytics-layer/advertising-service/src/main/java/propagandus/advertisingservice/core/models/enumerators/EAttentionLevel.java
+++
@@ -1,5 +0,0 @@
-package propagandus.advertisingservice.core.models.enumerators;
-
-public enum EAttentionLevel {
-  NO_INTEREST, FEEL_INTERESTED, INTERESTED, SOME_INTEREST, VERY_INTERESTED, EXTREM_INTEREST
-}
--- service/analytics-layer/advertising-service/src/main/java/propagandus/advertisingservice/core/models/enumerators/EPackageType.java
+++
@@ -1,5 +0,0 @@
-package propagandus.advertisingservice.core.models.enumerators;
-
-public enum EPackageType {
-  SIMPLE, MEDIUM, DEFAULT, ADVANCED
-}
