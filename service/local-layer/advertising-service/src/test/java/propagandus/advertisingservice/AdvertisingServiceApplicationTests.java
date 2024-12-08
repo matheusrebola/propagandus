ac53a8b735c1b3c378178bcfae979e72e9a5ccad
matheusrebola <matheusrebola@outlook.com>
2024-12-08 20:29:59 -0300

    feat: atualização dos microservices para Java

---
+++ service/local-layer/advertising-service/src/test/java/propagandus/advertisingservice/AdvertisingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package propagandus.advertisingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvertisingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ service/local-layer/data-persistence-service/src/test/java/propagandus/datapersistenceservice/DataPersistenceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package propagandus.datapersistenceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataPersistenceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
