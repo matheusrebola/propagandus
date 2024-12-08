2d7f7f285bebe544eebb0b79e7e69534438e05a7
matheusrebola <matheusrebola@outlook.com>
2024-12-08 16:45:05 -0300

    feat: remoção de microserviços desnecessários

--- service/local-layer/advertising-service/src/core/dtos/create-advertising.dto.ts
+++
@@ -1,6 +0,0 @@
-// src/products/dto/create-product.dto.ts
-export class CreateProductDto {
-  readonly name: string;
-  readonly description: string;
-  readonly price: number;
-}
--- service/local-layer/advertising-service/src/core/entities/advertising.model.ts
+++
@@ -1,7 +0,0 @@
-// src/products/models/product.model.ts
-export class Advertising {
-  id: number;
-  name: string;
-  description: string;
-  price: number;
-}
