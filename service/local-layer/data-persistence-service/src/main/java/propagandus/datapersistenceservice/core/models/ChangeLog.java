1f07af4c4484e43213c2c3bdad51feba4b02c234
matheusrebola <matheusrebola@outlook.com>
2024-12-09 21:37:55 -0300

    feat: remoção das anotações @NotNull desnecessárias

--- service/local-layer/data-persistence-service/src/main/java/propagandus/datapersistenceservice/core/models/ChangeLog.java
+++ service/local-layer/data-persistence-service/src/main/java/propagandus/datapersistenceservice/core/models/ChangeLog.java
@@ -10,17 +10,16 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotNull;
 import propagandus.datapersistenceservice.core.models.enumerators.EActionType;
 
 @Entity(name = "change_log")
 @Table(name = "change_log")
 public record ChangeLog(
   @Id @GeneratedValue(strategy = GenerationType.AUTO) @Column(name = "advertising_id") Long id,
-  @Column(name = "table_name", length = 20, nullable = false) @NotNull String tableName,
-  @Enumerated(EnumType.STRING) @Column(name = "action_type", length = 7, nullable = false) @NotNull EActionType actionType,
-  @Column(name = "primary_key_value", length = 20, nullable = false) @NotNull String primaryKeyValue,
-  @Column(name = "changed_at", length = 20, nullable = false) @NotNull LocalDateTime changedAt
+  @Column(name = "table_name", length = 20, nullable = false) String tableName,
+  @Enumerated(EnumType.STRING) @Column(name = "action_type", length = 7, nullable = false) EActionType actionType,
+  @Column(name = "primary_key_value", length = 20, nullable = false) String primaryKeyValue,
+  @Column(name = "changed_at", length = 20, nullable = false) LocalDateTime changedAt
 ) {
 
 }
