2c2c4fbc1c9d976ec53709c6cbe37585af5b1d61
matheusrebola <matheusrebola@outlook.com>
2024-12-10 11:26:07 -0300

    feat: adição da interface de view na repository

--- service/analytics-layer/location-service/src/main/java/propagandus/locationservice/core/repositorys/LocationRepository.java
+++ service/analytics-layer/location-service/src/main/java/propagandus/locationservice/core/repositorys/LocationRepository.java
@@ -4,7 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import propagandus.locationservice.core.models.Location;
+import propagandus.locationservice.core.views.LocationView;
 
 @Repository
-public interface LocationRepository extends JpaRepository<Location, Long> {
+public interface LocationRepository extends JpaRepository<Location, Long>, LocationView {
 }
--- service/analytics-layer/reaction-service/src/main/java/propagandus/reactionservice/core/repositorys/ReactionRepository.java
+++ service/analytics-layer/reaction-service/src/main/java/propagandus/reactionservice/core/repositorys/ReactionRepository.java
@@ -4,7 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import propagandus.reactionservice.core.models.Reaction;
+import propagandus.reactionservice.core.views.ReactionView;
 
 @Repository
-public interface ReactionRepository extends JpaRepository<Reaction, Long> {
+public interface ReactionRepository extends JpaRepository<Reaction, Long>, ReactionView {
 }
