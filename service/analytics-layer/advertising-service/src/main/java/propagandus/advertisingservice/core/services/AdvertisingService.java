5fe5b082774f262060c25b602689d2f0958561d4
matheusrebola <matheusrebola@outlook.com>
2024-12-10 11:28:02 -0300

    feat: configuração das services com os métodos da view + método de verificação de existência por ID

--- service/analytics-layer/advertising-service/src/main/java/propagandus/advertisingservice/core/services/AdvertisingService.java
+++ service/analytics-layer/advertising-service/src/main/java/propagandus/advertisingservice/core/services/AdvertisingService.java
@@ -14,13 +14,15 @@ import propagandus.advertisingservice.core.repositorys.AdvertisingRepository;
 public class AdvertisingService {
   private final AdvertisingRepository advertisingRepository;
 
+  //repository
   public List<Advertising> findAll(){return advertisingRepository.findAll();}
   public Advertising findById(Long id){return advertisingRepository.findById(id).orElse(null);}
-  public Advertising save(Advertising advertising){return advertisingRepository.save(advertising);}
   public boolean exists(Long id){return advertisingRepository.existsById(id);}
   public String findByName(String name){return advertisingRepository.findByName(name);}
   public List<String> findByCompany(String company){return advertisingRepository.findByCompany(company);}
   public List<String> findByProduct(String product){return advertisingRepository.findByProduct(product);}
+
+  //view
   public List<Advertising> findByReactionType(EReactionType reactionType){return advertisingRepository.findByReactionType(reactionType);}
   public List<Advertising> findPositiveReactionsByCompany(String company){return advertisingRepository.findPositiveReactionsByCompany(company);}
   public List<Advertising> findByCityWithReactions(String city){return findByCityWithReactions(city);}
--- service/analytics-layer/location-service/src/main/java/propagandus/locationservice/core/services/LocationService.java
+++ service/analytics-layer/location-service/src/main/java/propagandus/locationservice/core/services/LocationService.java
@@ -13,7 +13,25 @@ import propagandus.locationservice.core.repositorys.LocationRepository;
 public class LocationService {
   private final LocationRepository locationRepository;
 
+  //repository
   public List<Location> findAll(){return locationRepository.findAll();}
   public Location findById(Long id){return locationRepository.findById(id).orElse(null);}
-  public Location save(Location location){return locationRepository.save(location);}
+  public boolean existsByID(Long id){return locationRepository.existsById(id);}
+
+  //views
+  public List<Object[]> findLocationsWithMostReactions(){return locationRepository.findLocationsWithMostReactions();}
+  public List<Location> findLocationsWithAdvertisements(){return locationRepository.findLocationsWithAdvertisements();}
+  public List<Location> findLocationsByCityWithPositiveReactions(String city){return locationRepository.findLocationsByCityWithPositiveReactions(city);}
+  public List<Location> findLocationsWithReactions(){return locationRepository.findLocationsWithReactions();}
+  public List<Object[]> countAdvertisementsByState(){return locationRepository.countAdvertisementsByState();}
+  public List<Location> findLocationsByCountryWithNegativeReactions(String country){return locationRepository.findLocationsByCountryWithNegativeReactions(country);}
+  public List<Location> findLocationsWithAdvertisementsByCompany(String company){return locationRepository.findLocationsWithAdvertisementsByCompany(company);}
+  public List<Object[]> countReactionsByTypeInCity(String city){return locationRepository.countReactionsByTypeInCity(city);}
+  public List<Object[]> findLocationsWithAdvertisementsAndReactions(){return locationRepository.findLocationsWithAdvertisementsAndReactions();}
+  public List<Location> findLocationsWithReactionsByProduct(String product){return locationRepository.findLocationsWithReactionsByProduct(product);}
+  public List<Object[]> countReactionsByState(){return locationRepository.countReactionsByState();}
+  public List<Location> findLocationsWithMoreThanXReactionsForAdvertising(String advertisingName, Long reactionCount){return locationRepository.findLocationsWithMoreThanXReactionsForAdvertising(advertisingName, reactionCount);}
+  public List<Location> findLocationsWithAdvertisementsAndReactionsByState(String state){return locationRepository.findLocationsWithAdvertisementsAndReactionsByState(state);}
+  public List<Object[]> countPositiveReactionsByCity(){return locationRepository.countPositiveReactionsByCity();}
+  public List<Location> findLocationsWithoutReactions(){return locationRepository.findLocationsWithoutReactions();}
 }
