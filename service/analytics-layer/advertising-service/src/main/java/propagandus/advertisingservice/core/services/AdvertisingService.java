6560253026757091e10b4cc83db0a17086e8fd77
matheusrebola <matheusrebola@outlook.com>
2024-12-10 09:57:38 -0300

    feat: configuração dos métodos para buscar os dados pela service

--- service/analytics-layer/advertising-service/src/main/java/propagandus/advertisingservice/core/services/AdvertisingService.java
+++ service/analytics-layer/advertising-service/src/main/java/propagandus/advertisingservice/core/services/AdvertisingService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
 import propagandus.advertisingservice.core.models.Advertising;
+import propagandus.advertisingservice.core.models.enumerators.EReactionType;
 import propagandus.advertisingservice.core.repositorys.AdvertisingRepository;
 
 @Service
@@ -20,4 +21,17 @@ public class AdvertisingService {
   public String findByName(String name){return advertisingRepository.findByName(name);}
   public List<String> findByCompany(String company){return advertisingRepository.findByCompany(company);}
   public List<String> findByProduct(String product){return advertisingRepository.findByProduct(product);}
+  public List<Advertising> findByReactionType(EReactionType reactionType){return advertisingRepository.findByReactionType(reactionType);}
+  public List<Advertising> findPositiveReactionsByCompany(String company){return advertisingRepository.findPositiveReactionsByCompany(company);}
+  public List<Advertising> findByCityWithReactions(String city){return findByCityWithReactions(city);}
+  public List<Object[]> findAdvertisingDetailsWithReactions(String name){return advertisingRepository.findAdvertisingDetailsWithReactions(name);}
+  public List<Advertising> findByStateWithReactions(String state){return advertisingRepository.findByStateWithReactions(state);}
+  public List<Object[]> countReactionsByAdvertising(){return advertisingRepository.countReactionsByAdvertising();}
+  public List<Object[]> findAdvertisingWithMostReactionsInCity(String city){return advertisingRepository.findAdvertisingWithMostReactionsInCity(city);}
+  public List<Object[]> findAdvertisingWithMostReactionsByType(EReactionType reactionType){return advertisingRepository.findAdvertisingWithMostReactionsByType(reactionType);}
+  public List<Object[]> countReactionsByProduct(){return advertisingRepository.countReactionsByProduct();}
+  public List<Advertising> findAdvertisingByPainel(Long painelId){return advertisingRepository.findAdvertisingByPainel(painelId);}
+  public List<Object[]> countReactionsByAdvertisingAndCity(){return advertisingRepository.countReactionsByAdvertisingAndCity();}
+  public List<Advertising> findAdvertisingByProductWithReactionsAboveThreshold(String product, Long threshold){return advertisingRepository.findAdvertisingByProductWithReactionsAboveThreshold(product, threshold);}
+  public List<Advertising> findAdvertisingInLocationsWithMorePainels(Byte painelCount){return advertisingRepository.findAdvertisingInLocationsWithMorePainels(painelCount);}
 }
