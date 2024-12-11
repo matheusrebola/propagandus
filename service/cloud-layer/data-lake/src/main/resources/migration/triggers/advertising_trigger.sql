ffe9f965c062473e8efdbe36af27a317f9242d40
matheusrebola <matheusrebola@outlook.com>
2024-12-11 15:10:18 -0300

    feat: atualização do script com os atributos corretos + atualização do atributo changed_at recebendo a data e hora atual

--- service/cloud-layer/data-lake/src/main/resources/migration/triggers/advertising_trigger.sql
+++ service/cloud-layer/data-lake/src/main/resources/migration/triggers/advertising_trigger.sql
@@ -1,27 +1,23 @@
-DELIMITER $$
-
 CREATE TRIGGER advertising_after_insert
 AFTER INSERT ON advertising_table
 FOR EACH ROW
 BEGIN
-    INSERT INTO change_log (table_name, action, primary_key_value)
-    VALUES ('advertising_table', 'INSERT', OLD.id);
-END$$
+    INSERT INTO change_log (table_name, action_type, primary_key_value, changed_at)
+    VALUES ('advertising_table', 'INSERT', NEW.advertising_id, NOW());
+END;
 
 CREATE TRIGGER advertising_after_update
 AFTER UPDATE ON advertising_table
 FOR EACH ROW
 BEGIN
-    INSERT INTO change_log (table_name, action, primary_key_value)
-    VALUES ('advertising_table', 'UPDATE', OLD.id);
-END$$
+    INSERT INTO change_log (table_name, action_type, primary_key_value, changed_at)
+    VALUES ('advertising_table', 'UPDATE', NEW.advertising_id, NOW());
+END;
 
-CREATE TRIGGER avertising_after_delete
+CREATE TRIGGER advertising_after_delete
 AFTER DELETE ON advertising_table
 FOR EACH ROW
 BEGIN
-    INSERT INTO change_log (table_name, action, primary_key_value)
-    VALUES ('advertising', 'DELETE', OLD.id);
-END$$
-
-DELIMITER ;
+    INSERT INTO change_log (table_name, action_type, primary_key_value, changed_at)
+    VALUES ('advertising_table', 'DELETE', OLD.advertising_id, NOW());
+END;
--- service/cloud-layer/data-lake/src/main/resources/migration/triggers/attention_trigger.sql
+++ service/cloud-layer/data-lake/src/main/resources/migration/triggers/attention_trigger.sql
@@ -1,27 +1,23 @@
-DELIMITER $$
-
 CREATE TRIGGER attention_after_insert
 AFTER INSERT ON attention_table
 FOR EACH ROW
 BEGIN
-    INSERT INTO change_log (table_name, action, primary_key_value)
-    VALUES ('attention_table', 'INSERT', OLD.id);
-END$$
+    INSERT INTO change_log (table_name, action_type, primary_key_value, changed_at)
+    VALUES ('attention_table', 'INSERT', NEW.attention_id, NOW());
+END;
 
 CREATE TRIGGER attention_after_update
 AFTER UPDATE ON attention_table
 FOR EACH ROW
 BEGIN
-    INSERT INTO change_log (table_name, action, primary_key_value)
-    VALUES ('attention_table', 'UPDATE', OLD.id);
-END$$
+    INSERT INTO change_log (table_name, action_type, primary_key_value, changed_at)
+    VALUES ('attention_table', 'UPDATE', OLD.attention_id, NOW());
+END;
 
 CREATE TRIGGER attention_after_delete
 AFTER DELETE ON attention_table
 FOR EACH ROW
 BEGIN
-    INSERT INTO change_log (table_name, action, primary_key_value)
-    VALUES ('attention_table', 'DELETE', OLD.id);
-END$$
-
-DELIMITER ;
+    INSERT INTO change_log (table_name, action_type, primary_key_value, changed_at)
+    VALUES ('attention_table', 'DELETE', OLD.attention_id, NOW());
+END;
