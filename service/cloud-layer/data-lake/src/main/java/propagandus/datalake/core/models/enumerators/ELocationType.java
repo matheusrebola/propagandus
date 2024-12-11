76ed83209c94194828f1de19bb6d91df71c52d5b
matheusrebola <matheusrebola@outlook.com>
2024-12-11 16:05:15 -0300

    feat: atualização das models

---
+++ service/cloud-layer/data-lake/src/main/java/propagandus/datalake/core/models/enumerators/ELocationType.java
@@ -0,0 +1,5 @@
+package propagandus.datalake.core.models.enumerators;
+
+public enum ELocationType {
+  BUS, TRAIN, SUBWAY, SUPERMARKET, MALL, AIRPORT, ELEVATOR
+}
--- service/cloud-layer/data-lake/src/main/java/propagandus/datalake/core/models/enumerators/EReactionType.java
+++ service/cloud-layer/data-lake/src/main/java/propagandus/datalake/core/models/enumerators/EReactionType.java
@@ -1,5 +1,5 @@
 package propagandus.datalake.core.models.enumerators;
 
 public enum EReactionType {
- VERY_SAD, SAD, NEUTRAL, HAPPY, VERY_HAPPY
+ NEGATIVE, NEUTRAL, POSITIVE
 }
