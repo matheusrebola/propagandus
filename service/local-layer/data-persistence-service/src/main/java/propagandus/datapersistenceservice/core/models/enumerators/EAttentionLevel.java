0004619e17c3c4415a3f2f8606fc53d932209866
matheusrebola <matheusrebola@outlook.com>
2024-12-11 16:05:31 -0300

    feat: atualização das models

--- service/local-layer/data-persistence-service/src/main/java/propagandus/datapersistenceservice/core/models/enumerators/EAttentionLevel.java
+++ service/local-layer/data-persistence-service/src/main/java/propagandus/datapersistenceservice/core/models/enumerators/EAttentionLevel.java
@@ -1,5 +1,5 @@
 package propagandus.datapersistenceservice.core.models.enumerators;
 
 public enum EAttentionLevel {
-  NO_INTEREST, FEEL_INTERESTED, INTERESTED, SOME_INTEREST, VERY_INTERESTED, EXTREM_INTEREST
+  HIGH, MEDIUM, LOW, CRITICAL, URGENT, NORMAL, MINIMAL, SIGNIFICANT, IMPORTANT, ESSENTIAL
 }
--- service/local-layer/data-persistence-service/src/main/resources/static/advertising_sequence_data.sql
+++
@@ -1,4 +0,0 @@
--- Insert into advertising_static_table
-INSERT INTO advertising_table (advertising_id, name, version, company, product, exibition_time) VALUES
-(1, 'AdName1', 'v1', 'CompanyA', 'ProductX', 300),
-(2, 'AdName2', 'v2', 'CompanyB', 'ProductY', 180);
