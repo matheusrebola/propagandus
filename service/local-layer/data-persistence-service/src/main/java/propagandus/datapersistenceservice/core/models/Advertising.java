ef182612504de42f8bb3d822a83960153f148a7d
matheusrebola <matheusrebola@outlook.com>
2024-12-11 21:59:18 -0300

    feat: criada a relação de um para um nas entidades de advertising

--- service/local-layer/data-persistence-service/src/main/java/propagandus/datapersistenceservice/core/models/Advertising.java
+++ service/local-layer/data-persistence-service/src/main/java/propagandus/datapersistenceservice/core/models/Advertising.java
@@ -11,16 +11,14 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity(name = "advertising")
 @Table(name = "advertising_table")
 public record Advertising(
   @Id @GeneratedValue(strategy = GenerationType.AUTO) @Column(name = "advertising_id") Long id,
-  @Column(length = 20, nullable = false, name = "advertising_name") String name,
-  @Column(length = 4, nullable = false, name = "advertising_version") String version,
-  @Column(length = 50, nullable = false) String company,
-  @Column(length = 20, nullable = false) String product,
+  @OneToOne @JoinColumn(name = "advertising_id", referencedColumnName = "advertising_info_id", nullable = false) AdvertisingInfo advertising,
   @OneToMany(mappedBy = "advertising", cascade = CascadeType.ALL, orphanRemoval = true) List<Reaction> reactions,
   @OneToMany(mappedBy = "advertising", cascade = CascadeType.ALL, orphanRemoval = true) List<PeriodRegister> advertisingTime,
   @ManyToOne @JoinColumn(name = "painel_id", nullable = false) Painel painel
