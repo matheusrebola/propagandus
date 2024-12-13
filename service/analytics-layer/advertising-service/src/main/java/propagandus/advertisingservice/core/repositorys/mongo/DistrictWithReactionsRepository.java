94ee724f9a10ec69b2ce9afc1e51b53eca7fcf0f
matheusrebola <matheusrebola@outlook.com>
2024-12-13 19:47:25 -0300

    feat: atualização do nome do document

--- service/analytics-layer/advertising-service/src/main/java/propagandus/advertisingservice/core/repositorys/mongo/DistrictWithReactionsRepository.java
+++ service/analytics-layer/advertising-service/src/main/java/propagandus/advertisingservice/core/repositorys/mongo/DistrictWithReactionsRepository.java
@@ -3,9 +3,9 @@ package propagandus.advertisingservice.core.repositorys.mongo;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import propagandus.advertisingservice.core.documents.DistrictWithReactions;
+import propagandus.advertisingservice.core.documents.StatetWithReactions;
 
 @Repository
-public interface DistrictWithReactionsRepository extends MongoRepository<DistrictWithReactions,Long> {
+public interface DistrictWithReactionsRepository extends MongoRepository<StatetWithReactions,Long> {
 
 }
