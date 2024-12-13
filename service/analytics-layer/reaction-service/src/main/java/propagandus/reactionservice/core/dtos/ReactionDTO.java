495de1f4411a9d668c1636fa5ca85654a9239a1f
matheusrebola <matheusrebola@outlook.com>
2024-12-13 18:48:27 -0300

    refator: remoção de DTO desnecessário + renomeado o DTO de period register

--- service/analytics-layer/reaction-service/src/main/java/propagandus/reactionservice/core/dtos/ReactionDTO.java
+++ service/analytics-layer/reaction-service/src/main/java/propagandus/reactionservice/core/dtos/ReactionDTO.java
@@ -2,7 +2,8 @@ package propagandus.reactionservice.core.dtos;
 
 import propagandus.reactionservice.core.models.enumerators.EReactionType;
 
-public record ReactionCreateDto(
+public record ReactionDTO(
+  Long reactionId,
   Long advertisingId,
   Long locationId,
   EReactionType reactionType
