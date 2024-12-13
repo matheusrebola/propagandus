a704c51b6ca6500059347a630c4116c252577598
matheusrebola <matheusrebola@outlook.com>
2024-12-13 17:06:04 -0300

    feat: atualização no tipo do retorno de cada query

--- service/analytics-layer/advertising-service/src/main/java/propagandus/advertisingservice/core/repositorys/AdvertisingRepository.java
+++ service/analytics-layer/advertising-service/src/main/java/propagandus/advertisingservice/core/repositorys/AdvertisingRepository.java
@@ -7,6 +7,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import propagandus.advertisingservice.core.dtos.AdvertisingCityReactionCountDTO;
+import propagandus.advertisingservice.core.dtos.AdvertisingDTO;
+import propagandus.advertisingservice.core.dtos.AdvertisingDetailsDTO;
+import propagandus.advertisingservice.core.dtos.AdvertisingReactionCountDTO;
+import propagandus.advertisingservice.core.dtos.AdvertisingReactionTypeCountDTO;
+import propagandus.advertisingservice.core.dtos.ProductReactionCountDTO;
 import propagandus.advertisingservice.core.models.Advertising;
 import propagandus.advertisingservice.core.models.enumerators.EMonth;
 import propagandus.advertisingservice.core.models.enumerators.EReactionType;
@@ -19,39 +25,39 @@ public interface AdvertisingRepository extends JpaRepository<Advertising,Long>{
   List<String> findByProduct(String product);
   //Listar todas as propagandas com pelo menos uma reação específica
   @Query("SELECT DISTINCT a FROM advertising a JOIN a.reactions r WHERE r.reactionType = :reactionType")
-  List<Advertising> findByReactionType(@Param("reactionType") EReactionType reactionType);
+  List<AdvertisingDTO> findByReactionType(@Param("reactionType") EReactionType reactionType);
 
   //Obter todas as propagandas de uma empresa específica com reações positivas
   @Query("SELECT a FROM advertising a JOIN a.reactions r WHERE a.company = :company AND r.reactionType = 'POSITIVE'")
-  List<Advertising> findPositiveReactionsByCompany(@Param("company") String company);
+  List<AdvertisingDTO> findPositiveReactionsByCompany(@Param("company") String company);
 
   //Buscar todas as propagandas que têm reações em uma cidade específica
   @Query("SELECT DISTINCT a FROM advertising a JOIN a.reactions r WHERE r.location.city = :city")
-  List<Advertising> findByCityWithReactions(@Param("city") String city);
+  List<AdvertisingDTO> findByCityWithReactions(@Param("city") String city);
 
   //Obter detalhes de uma propaganda específica incluindo as reações
   @Query("SELECT a, r FROM advertising a LEFT JOIN a.reactions r WHERE a.name = :name")
-  List<Object[]> findAdvertisingDetailsWithReactions(@Param("name") String name);
+  List<AdvertisingDetailsDTO> findAdvertisingDetailsWithReactions(@Param("name") String name);
 
   //Obter todas as propagandas com reações em um estado específico
   @Query("SELECT DISTINCT a FROM advertising a JOIN a.reactions r WHERE r.location.state = :state")
-  List<Advertising> findByStateWithReactions(@Param("state") String state);
+  List<AdvertisingDTO> findByStateWithReactions(@Param("state") String state);
 
   //Contar o número total de reações por propaganda
   @Query("SELECT a, COUNT(r) AS totalReactions FROM advertising a JOIN a.reactions r GROUP BY a ORDER BY totalReactions DESC")
-  List<Object[]> countReactionsByAdvertising();
+  List<AdvertisingReactionCountDTO> countReactionsByAdvertising();
 
   //Buscar propagandas com maior número de reações em uma cidade específica
   @Query("SELECT a, COUNT(r) AS totalReactions FROM advertising a JOIN a.reactions r JOIN r.location l WHERE l.city = :city GROUP BY a ORDER BY totalReactions DESC")
-  List<Object[]> findAdvertisingWithMostReactionsInCity(@Param("city") String city);
+  List<AdvertisingCityReactionCountDTO> findAdvertisingWithMostReactionsInCity(@Param("city") String city);
 
   //Obter as propagandas com mais reações positivas (ou qualquer outro tipo de reação)
   @Query("SELECT a, COUNT(r) AS positiveReactions FROM advertising a JOIN a.reactions r WHERE r.reactionType = :reactionType GROUP BY a ORDER BY positiveReactions DESC")
-  List<Object[]> findAdvertisingWithMostReactionsByType(@Param("reactionType") EReactionType reactionType);
+  List<AdvertisingReactionTypeCountDTO> findAdvertisingWithMostReactionsByType(@Param("reactionType") EReactionType reactionType);
 
   //Contar o número total de reações por produto
   @Query("SELECT a.product, COUNT(r) AS totalReactions FROM advertising a JOIN a.reactions r GROUP BY a.product ORDER BY totalReactions DESC")
-  List<Object[]> countReactionsByProduct();
+  List<ProductReactionCountDTO> countReactionsByProduct();
 
   //Buscar propagandas que receberam reações em um período específico
   @Query("SELECT a FROM advertising a JOIN a.reactions r JOIN period_register p ON r.id = p.id WHERE p.month = :month AND p.year = :year")
@@ -59,19 +65,19 @@ public interface AdvertisingRepository extends JpaRepository<Advertising,Long>{
 
   //Listar as propagandas com reações associadas a um painel específico
   @Query("SELECT DISTINCT a FROM advertising a JOIN a.reactions r JOIN r.location l JOIN l.painels p WHERE p.id = :painelId")
-  List<Advertising> findAdvertisingByPainel(@Param("painelId") Long painelId);
+  List<AdvertisingDTO> findAdvertisingByPainel(@Param("painelId") Long painelId);
 
   //Obter o total de reações por cidade para cada propaganda
   @Query("SELECT a, l.city, COUNT(r) AS totalReactions FROM advertising a JOIN a.reactions r JOIN r.location l GROUP BY a, l.city ORDER BY totalReactions DESC")
-  List<Object[]> countReactionsByAdvertisingAndCity();
+  List<ProductReactionCountDTO> countReactionsByAdvertisingAndCity();
 
   //Buscar propagandas de um produto específico que tenham mais de X reações
   @Query("SELECT a FROM advertising a JOIN a.reactions r WHERE a.product = :product GROUP BY a HAVING COUNT(r) > :threshold")
-  List<Advertising> findAdvertisingByProductWithReactionsAboveThreshold(@Param("product") String product, @Param("threshold") Long threshold);
+  List<AdvertisingDTO> findAdvertisingByProductWithReactionsAboveThreshold(@Param("product") String product, @Param("threshold") Long threshold);
 
   //Listar propagandas que geraram reações em locais com mais de N painéis
   @Query("SELECT a FROM advertising a JOIN a.reactions r JOIN r.location l WHERE SIZE(l.painels) > :painelCount")
-  List<Advertising> findAdvertisingInLocationsWithMorePainels(@Param("painelCount") Byte painelCount);
+  List<AdvertisingDTO> findAdvertisingInLocationsWithMorePainels(@Param("painelCount") Byte painelCount);
 
   //@Query("SELECT pr.period_of_the_day, COUNT(DISTINCT pr.advertising_id) AS total_advertisings FROM date_time_table pr GROUP BY pr.period_of_the_day")
   //List<Object[]> totalAdvertisingyPeriodOfTheDay();
--- service/analytics-layer/advertising-service/src/main/java/propagandus/advertisingservice/core/services/AdvertisingService.java
+++ service/analytics-layer/advertising-service/src/main/java/propagandus/advertisingservice/core/services/AdvertisingService.java
@@ -5,6 +5,12 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+import propagandus.advertisingservice.core.dtos.AdvertisingCityReactionCountDTO;
+import propagandus.advertisingservice.core.dtos.AdvertisingDTO;
+import propagandus.advertisingservice.core.dtos.AdvertisingDetailsDTO;
+import propagandus.advertisingservice.core.dtos.AdvertisingReactionCountDTO;
+import propagandus.advertisingservice.core.dtos.AdvertisingReactionTypeCountDTO;
+import propagandus.advertisingservice.core.dtos.ProductReactionCountDTO;
 import propagandus.advertisingservice.core.models.Advertising;
 import propagandus.advertisingservice.core.models.enumerators.EReactionType;
 import propagandus.advertisingservice.core.repositorys.AdvertisingRepository;
@@ -22,17 +28,17 @@ public class AdvertisingService {
   public List<String> findByProduct(String product){return advertisingRepository.findByProduct(product);}
 
   //view
-  public List<Advertising> findByReactionType(EReactionType reactionType){return advertisingRepository.findByReactionType(reactionType);}
-  public List<Advertising> findPositiveReactionsByCompany(String company){return advertisingRepository.findPositiveReactionsByCompany(company);}
-  public List<Advertising> findByCityWithReactions(String city){return findByCityWithReactions(city);}
-  public List<Object[]> findAdvertisingDetailsWithReactions(String name){return advertisingRepository.findAdvertisingDetailsWithReactions(name);}
-  public List<Advertising> findByStateWithReactions(String state){return advertisingRepository.findByStateWithReactions(state);}
-  public List<Object[]> countReactionsByAdvertising(){return advertisingRepository.countReactionsByAdvertising();}
-  public List<Object[]> findAdvertisingWithMostReactionsInCity(String city){return advertisingRepository.findAdvertisingWithMostReactionsInCity(city);}
-  public List<Object[]> findAdvertisingWithMostReactionsByType(EReactionType reactionType){return advertisingRepository.findAdvertisingWithMostReactionsByType(reactionType);}
-  public List<Object[]> countReactionsByProduct(){return advertisingRepository.countReactionsByProduct();}
-  public List<Advertising> findAdvertisingByPainel(Long painelId){return advertisingRepository.findAdvertisingByPainel(painelId);}
-  public List<Object[]> countReactionsByAdvertisingAndCity(){return advertisingRepository.countReactionsByAdvertisingAndCity();}
-  public List<Advertising> findAdvertisingByProductWithReactionsAboveThreshold(String product, Long threshold){return advertisingRepository.findAdvertisingByProductWithReactionsAboveThreshold(product, threshold);}
-  public List<Advertising> findAdvertisingInLocationsWithMorePainels(Byte painelCount){return advertisingRepository.findAdvertisingInLocationsWithMorePainels(painelCount);}
+  public List<AdvertisingDTO> findByReactionType(EReactionType reactionType){return advertisingRepository.findByReactionType(reactionType);}
+  public List<AdvertisingDTO> findPositiveReactionsByCompany(String company){return advertisingRepository.findPositiveReactionsByCompany(company);}
+  public List<AdvertisingDTO> findByCityWithReactions(String city){return findByCityWithReactions(city);}
+  public List<AdvertisingDetailsDTO> findAdvertisingDetailsWithReactions(String name){return advertisingRepository.findAdvertisingDetailsWithReactions(name);}
+  public List<AdvertisingDTO> findByStateWithReactions(String state){return advertisingRepository.findByStateWithReactions(state);}
+  public List<AdvertisingReactionCountDTO> countReactionsByAdvertising(){return advertisingRepository.countReactionsByAdvertising();}
+  public List<AdvertisingCityReactionCountDTO> findAdvertisingWithMostReactionsInCity(String city){return advertisingRepository.findAdvertisingWithMostReactionsInCity(city);}
+  public List<AdvertisingReactionTypeCountDTO> findAdvertisingWithMostReactionsByType(EReactionType reactionType){return advertisingRepository.findAdvertisingWithMostReactionsByType(reactionType);}
+  public List<ProductReactionCountDTO> countReactionsByProduct(){return advertisingRepository.countReactionsByProduct();}
+  public List<AdvertisingDTO> findAdvertisingByPainel(Long painelId){return advertisingRepository.findAdvertisingByPainel(painelId);}
+  public List<ProductReactionCountDTO> countReactionsByAdvertisingAndCity(){return advertisingRepository.countReactionsByAdvertisingAndCity();}
+  public List<AdvertisingDTO> findAdvertisingByProductWithReactionsAboveThreshold(String product, Long threshold){return advertisingRepository.findAdvertisingByProductWithReactionsAboveThreshold(product, threshold);}
+  public List<AdvertisingDTO> findAdvertisingInLocationsWithMorePainels(Byte painelCount){return advertisingRepository.findAdvertisingInLocationsWithMorePainels(painelCount);}
 }
