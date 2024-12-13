b6b36a1e2edfa729bab5e646e34270c55d96a682
matheusrebola <matheusrebola@outlook.com>
2024-12-13 11:01:38 -0300

    feat: configuração das rotas e das variáveis de caminho

--- service/analytics-layer/location-service/src/main/java/propagandus/locationservice/core/controllers/LocationController.java
+++ service/analytics-layer/location-service/src/main/java/propagandus/locationservice/core/controllers/LocationController.java
@@ -2,6 +2,7 @@ package propagandus.locationservice.core.controllers;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,14 +15,28 @@ import propagandus.locationservice.core.models.enumerators.ELocationType;
 @RequiredArgsConstructor
 public class LocationController {
   
-  public List<String> findByZipCode(String zipCode){return null;}
-  public List<String> findByPublicPlace(String publicPlace){return null;}
+
+  @GetMapping("{location-type}")
   public List<ELocationType> findByLocationType(ELocationType locationType){return null;}
+
+  @GetMapping("/reaction/{city}")
   public List<Location> findLocationsByCityWithPositiveReactions(String city){return null;}
+
+  @GetMapping("{country}")
   public List<Location> findLocationsByCountryWithNegativeReactions(String country){return null;}
+
+  @GetMapping("{company}")
   public List<Location> findLocationsWithAdvertisementsByCompany(String company){return null;}
+
+  @GetMapping("{city}")
   public List<Object[]> countReactionsByTypeInCity(String city){return null;}
+
+  @GetMapping("{product}")
   public List<Location> findLocationsWithReactionsByProduct(String product){return null;}
+
+  @GetMapping("/advertising/{advertising}/reaction/{reaction}")
   public List<Location> findLocationsWithMoreThanXReactionsForAdvertising(String advertisingName, Long reactionCount){return null;}
+
+  @GetMapping("{state}")
   public List<Location> findLocationsWithAdvertisementsAndReactionsByState(String state){return null;}
 }
