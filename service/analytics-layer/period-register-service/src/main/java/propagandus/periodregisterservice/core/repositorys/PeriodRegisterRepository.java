d01868256ac80f2ac732bfc7201c60bd13d9c1fb
matheusrebola <matheusrebola@outlook.com>
2024-12-13 18:41:26 -0300

    feat: atualização do retorno dos métodos com suas devidas DTOs

--- service/analytics-layer/period-register-service/src/main/java/propagandus/periodregisterservice/core/repositorys/PeriodRegisterRepository.java
+++ service/analytics-layer/period-register-service/src/main/java/propagandus/periodregisterservice/core/repositorys/PeriodRegisterRepository.java
@@ -9,6 +9,17 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import propagandus.periodregisterservice.core.dtos.DayWithMostRegistersDTO;
+import propagandus.periodregisterservice.core.dtos.PeakRegisterHourDTO;
+import propagandus.periodregisterservice.core.dtos.PeriodRegisterDTO;
+import propagandus.periodregisterservice.core.dtos.ReactionCountByMonthDTO;
+import propagandus.periodregisterservice.core.dtos.ReactionCountByPeriodDTO;
+import propagandus.periodregisterservice.core.dtos.RegisterCountByDateDTO;
+import propagandus.periodregisterservice.core.dtos.RegisterCountByDayDTO;
+import propagandus.periodregisterservice.core.dtos.RegisterCountByDayWeekAndPeriodDTO;
+import propagandus.periodregisterservice.core.dtos.RegisterCountByDayWeekDTO;
+import propagandus.periodregisterservice.core.dtos.RegisterCountByPeriodAndMonthDTO;
+import propagandus.periodregisterservice.core.dtos.RegisterCountByPeriodDTO;
 import propagandus.periodregisterservice.core.models.PeriodRegister;
 import propagandus.periodregisterservice.core.models.enumerators.EMonth;
 
@@ -16,71 +27,71 @@ import propagandus.periodregisterservice.core.models.enumerators.EMonth;
 public interface PeriodRegisterRepository extends JpaRepository<PeriodRegister, Long> {
  //Contar o número de reações por período do dia
   @Query("SELECT p.periodOfTheDay, COUNT(r) FROM period_register p JOIN reaction r ON p.id = r.id GROUP BY p.periodOfTheDay")
-  List<Object[]> countReactionsByPeriodOfTheDay();
+  List<ReactionCountByPeriodDTO> countReactionsByPeriodOfTheDay();
 
   //Contar o número de reações por mês
   @Query("SELECT p.month, COUNT(r) FROM period_register p JOIN reaction r ON p.id = r.id GROUP BY p.month ORDER BY p.month")
-  List<Object[]> countReactionsByMonth();
+  List<ReactionCountByMonthDTO> countReactionsByMonth();
 
   //Buscar registros em um período de tempo específico
   @Query("SELECT pr FROM period_register pr WHERE pr.registerTime BETWEEN :startTime AND :endTime")
-  List<PeriodRegister> findByTimeRange(@Param("startTime") LocalDateTime startTime, @Param("endTime") LocalDateTime endTime);
+  List<PeriodRegisterDTO> findByTimeRange(@Param("startTime") LocalDateTime startTime, @Param("endTime") LocalDateTime endTime);
 
   //Contar registros por dia da semana
   @Query("SELECT pr.dayWeek, COUNT(pr) AS registerCount FROM period_register pr GROUP BY pr.dayWeek ORDER BY registerCount DESC")
-  List<Object[]> countRegistersByDayOfWeek();
+  List<RegisterCountByDayWeekDTO> countRegistersByDayOfWeek();
 
   //Buscar registros em um determinado mês e ano
   @Query("SELECT pr FROM period_register pr WHERE pr.month = :month AND pr.year = :year")
-  List<PeriodRegister> findByMonthAndYear(@Param("month") EMonth month, @Param("year") String year);
+  List<PeriodRegisterDTO> findByMonthAndYear(@Param("month") EMonth month, @Param("year") String year);
 
   //Listar registros por período do dia
   @Query("SELECT pr.periodOfTheDay, COUNT(pr) AS registerCount FROM period_register pr GROUP BY pr.periodOfTheDay ORDER BY registerCount DESC")
-  List<Object[]> countRegistersByPeriodOfTheDay();
+  List<RegisterCountByPeriodDTO> countRegistersByPeriodOfTheDay();
 
   //Obter o horário com mais registros
   @Query("SELECT pr.hour, COUNT(pr) AS registerCount FROM period_register pr GROUP BY pr.hour ORDER BY registerCount DESC")
-  List<Object[]> findPeakRegisterHour();
+  List<PeakRegisterHourDTO> findPeakRegisterHour();
 
   //Contar registros por combinação de dia da semana e período do dia
   @Query("SELECT pr.dayWeek, pr.periodOfTheDay, COUNT(pr) AS registerCount FROM period_register pr GROUP BY pr.dayWeek, pr.periodOfTheDay ORDER BY registerCount DESC")
-  List<Object[]> countRegistersByDayWeekAndPeriod();
+  List<RegisterCountByDayWeekAndPeriodDTO> countRegistersByDayWeekAndPeriod();
 
   //Buscar registros realizados em dias úteis
   @Query("SELECT pr FROM period_register pr WHERE pr.dayWeek IN ('MON', 'TUE', 'WED', 'THU', 'FRI')")
-  List<PeriodRegister> findWeekdayRegisters();
+  List<PeriodRegisterDTO> findWeekdayRegisters();
 
   //Contar registros realizados aos finais de semana
   @Query("SELECT COUNT(pr) FROM period_register pr WHERE pr.dayWeek IN ('SAT', 'SUN')")
-  Long countWeekendRegisters();
+  Integer countWeekendRegisters();
 
   //Listar registros por ano, mês e dia
   @Query("SELECT pr.year, pr.month, pr.day, COUNT(pr) AS registerCount FROM period_register pr GROUP BY pr.year, pr.month, pr.day ORDER BY pr.year, pr.month, pr.day")
-  List<Object[]> countRegistersByYearMonthDay();
+  List<RegisterCountByDateDTO> countRegistersByYearMonthDay();
 
   //Buscar registros em um horário exato
   @Query("SELECT pr FROM period_register pr WHERE pr.hour = :hour AND pr.minute = :minute")
-  List<PeriodRegister> findByExactTime(@Param("hour") Byte hour, @Param("minute") Byte minute);
+  List<PeriodRegisterDTO> findByExactTime(@Param("hour") Byte hour, @Param("minute") Byte minute);
 
   //Obter registros em um mês específico ordenados por horário
   @Query("SELECT pr FROM period_register pr WHERE pr.month = :month ORDER BY pr.registerTime ASC")
-  List<PeriodRegister> findByMonthOrderedByTime(@Param("month") EMonth month);
+  List<PeriodRegisterDTO> findByMonthOrderedByTime(@Param("month") EMonth month);
 
   //Calcular o total de registros por período do dia e mês
   @Query("SELECT pr.periodOfTheDay, pr.month, COUNT(pr) AS registerCount FROM period_register pr GROUP BY pr.periodOfTheDay, pr.month ORDER BY registerCount DESC")
-  List<Object[]> countRegistersByPeriodAndMonth();
+  List<RegisterCountByPeriodAndMonthDTO> countRegistersByPeriodAndMonth();
 
   //Buscar o dia com mais registros em um mês específico
   @Query("SELECT pr.day, COUNT(pr) AS registerCount FROM period_register pr WHERE pr.month = :month GROUP BY pr.day ORDER BY registerCount DESC")
-  List<Object[]> findDayWithMostRegistersInMonth(@Param("month") EMonth month);
+  List<DayWithMostRegistersDTO> findDayWithMostRegistersInMonth(@Param("month") EMonth month);
 
   //Contar registros realizados em dias específicos do mês
   @Query("SELECT pr.day, COUNT(pr) AS registerCount FROM period_register pr WHERE pr.day IN (:days) GROUP BY pr.day ORDER BY pr.day ASC")
-  List<Object[]> countRegistersBySpecificDays(@Param("days") List<Byte> days);
+  List<RegisterCountByDayDTO> countRegistersBySpecificDays(@Param("days") List<Byte> days);
 
   //Obter os registros mais recentes
   @Query("SELECT pr FROM period_register pr ORDER BY pr.registerTime DESC")
-  List<PeriodRegister> findMostRecentRegisters(Pageable pageable);
+  List<PeriodRegisterDTO> findMostRecentRegisters(Pageable pageable);
   
   /*@Query("""
     SELECT 
--- service/analytics-layer/period-register-service/src/main/java/propagandus/periodregisterservice/core/services/PeriodRegisterService.java
+++ service/analytics-layer/period-register-service/src/main/java/propagandus/periodregisterservice/core/services/PeriodRegisterService.java
@@ -3,10 +3,21 @@ package propagandus.periodregisterservice.core.services;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
-import propagandus.periodregisterservice.core.models.PeriodRegister;
+import propagandus.periodregisterservice.core.dtos.DayWithMostRegistersDTO;
+import propagandus.periodregisterservice.core.dtos.PeakRegisterHourDTO;
+import propagandus.periodregisterservice.core.dtos.PeriodRegisterDTO;
+import propagandus.periodregisterservice.core.dtos.ReactionCountByMonthDTO;
+import propagandus.periodregisterservice.core.dtos.ReactionCountByPeriodDTO;
+import propagandus.periodregisterservice.core.dtos.RegisterCountByDateDTO;
+import propagandus.periodregisterservice.core.dtos.RegisterCountByDayDTO;
+import propagandus.periodregisterservice.core.dtos.RegisterCountByDayWeekAndPeriodDTO;
+import propagandus.periodregisterservice.core.dtos.RegisterCountByDayWeekDTO;
+import propagandus.periodregisterservice.core.dtos.RegisterCountByPeriodAndMonthDTO;
+import propagandus.periodregisterservice.core.dtos.RegisterCountByPeriodDTO;
 import propagandus.periodregisterservice.core.models.enumerators.EMonth;
 import propagandus.periodregisterservice.core.repositorys.PeriodRegisterRepository;
 
@@ -19,20 +30,21 @@ public class PeriodRegisterService {
   public boolean existsById(Long id){return periodRegisterRepository.existsById(id);}
 
   //view
-  public List<Object[]> countReactionsByPeriodOfTheDay(){return periodRegisterRepository.countReactionsByPeriodOfTheDay();}
-  public List<Object[]> countReactionsByMonth(){return periodRegisterRepository.countReactionsByMonth();}
-  public List<PeriodRegister> findByTimeRange(LocalDateTime startTime, LocalDateTime endTime){return periodRegisterRepository.findByTimeRange(startTime, endTime);}
-  public List<Object[]> countRegistersByDayOfWeek(){return periodRegisterRepository.countRegistersByDayOfWeek();}
-  public List<PeriodRegister> findByMonthAndYear(EMonth month, String year){return periodRegisterRepository.findByMonthAndYear(month, year);}
-  public List<Object[]> countRegistersByPeriodOfTheDay(){return periodRegisterRepository.countRegistersByPeriodOfTheDay();}
-  public List<Object[]> findPeakRegisterHour(){return periodRegisterRepository.findPeakRegisterHour();}
-  public List<Object[]> countRegistersByDayWeekAndPeriod(){return periodRegisterRepository.countRegistersByDayWeekAndPeriod();}
-  public List<PeriodRegister> findWeekdayRegisters(){return periodRegisterRepository.findWeekdayRegisters();}
-  public Long countWeekendRegisters(){return periodRegisterRepository.countWeekendRegisters();}
-  public List<Object[]> countRegistersByYearMonthDay(){return periodRegisterRepository.countRegistersByYearMonthDay();}
-  public List<PeriodRegister> findByExactTime(Byte hour, Byte minute){return periodRegisterRepository.findByExactTime(hour, minute);}
-  public List<PeriodRegister> findByMonthOrderedByTime(EMonth month){return periodRegisterRepository.findByMonthOrderedByTime(month);}
-  public List<Object[]> countRegistersByPeriodAndMonth(){return periodRegisterRepository.countRegistersByPeriodAndMonth();}
-  public List<Object[]> findDayWithMostRegistersInMonth(EMonth month){return periodRegisterRepository.findDayWithMostRegistersInMonth(month);}
-  public List<Object[]> countRegistersBySpecificDays(List<Byte> days){return periodRegisterRepository.countRegistersBySpecificDays(days);}
+  public List<ReactionCountByPeriodDTO> countReactionsByPeriodOfTheDay(){return periodRegisterRepository.countReactionsByPeriodOfTheDay();}
+  public List<ReactionCountByMonthDTO> countReactionsByMonth(){return periodRegisterRepository.countReactionsByMonth();}
+  public List<PeriodRegisterDTO> findByTimeRange(LocalDateTime startTime, LocalDateTime endTime){return periodRegisterRepository.findByTimeRange(startTime, endTime);}
+  public List<RegisterCountByDayWeekDTO> countRegistersByDayOfWeek(){return periodRegisterRepository.countRegistersByDayOfWeek();}
+  public List<PeriodRegisterDTO> findByMonthAndYear(EMonth month, String year){return periodRegisterRepository.findByMonthAndYear(month, year);}
+  public List<RegisterCountByPeriodDTO> countRegistersByPeriodOfTheDay(){return periodRegisterRepository.countRegistersByPeriodOfTheDay();}
+  public List<PeakRegisterHourDTO> findPeakRegisterHour(){return periodRegisterRepository.findPeakRegisterHour();}
+  public List<RegisterCountByDayWeekAndPeriodDTO> countRegistersByDayWeekAndPeriod(){return periodRegisterRepository.countRegistersByDayWeekAndPeriod();}
+  public List<PeriodRegisterDTO> findWeekdayRegisters(){return periodRegisterRepository.findWeekdayRegisters();}
+  public Integer countWeekendRegisters(){return periodRegisterRepository.countWeekendRegisters();}
+  public List<RegisterCountByDateDTO> countRegistersByYearMonthDay(){return periodRegisterRepository.countRegistersByYearMonthDay();}
+  public List<PeriodRegisterDTO> findByExactTime(Byte hour, Byte minute){return periodRegisterRepository.findByExactTime(hour, minute);}
+  public List<PeriodRegisterDTO> findByMonthOrderedByTime(EMonth month){return periodRegisterRepository.findByMonthOrderedByTime(month);}
+  public List<RegisterCountByPeriodAndMonthDTO> countRegistersByPeriodAndMonth(){return periodRegisterRepository.countRegistersByPeriodAndMonth();}
+  public List<DayWithMostRegistersDTO> findDayWithMostRegistersInMonth(EMonth month){return periodRegisterRepository.findDayWithMostRegistersInMonth(month);}
+  public List<RegisterCountByDayDTO> countRegistersBySpecificDays(List<Byte> days){return periodRegisterRepository.countRegistersBySpecificDays(days);}
+  public List<PeriodRegisterDTO> findMostRecentRegisters(Pageable pageable){return periodRegisterRepository.findMostRecentRegisters(pageable);}
 }
