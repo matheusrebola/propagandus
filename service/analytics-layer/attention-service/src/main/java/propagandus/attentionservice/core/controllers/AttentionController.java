0bae2a733481ab1b904c9398630443cc33b581c3
matheusrebola <matheusrebola@outlook.com>
2024-12-13 10:55:02 -0300

    feat: configuração das rotas e das variáveis de caminho

--- service/analytics-layer/attention-service/src/main/java/propagandus/attentionservice/core/controllers/AttentionController.java
+++ service/analytics-layer/attention-service/src/main/java/propagandus/attentionservice/core/controllers/AttentionController.java
@@ -3,11 +3,12 @@ package propagandus.attentionservice.core.controllers;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
-import propagandus.attentionservice.core.models.Attention;
 import propagandus.attentionservice.core.models.Painel;
 import propagandus.attentionservice.core.models.enumerators.EAttentionLevel;
 import propagandus.attentionservice.core.models.enumerators.EDayWeek;
@@ -18,25 +19,59 @@ import propagandus.attentionservice.core.models.enumerators.EPeriodOfTheDay;
 @RequestMapping("/attention")
 @RequiredArgsConstructor
 public class AttentionController {
+
+  @GetMapping("{attention-level}")
+  List<EAttentionLevel> findByAttention(@PathVariable EAttentionLevel attention){return null;}
+
+  @GetMapping("{attention}")
+  public List<Byte> findByAttentionLevel(@PathVariable Byte attention){return null;}
+
+  @GetMapping("{look-count}")
+  public List<Short> findByLookCount(@PathVariable Short lookCount){return null;}
+
+  @GetMapping("{people-count}")
+  public List<Short> findByPeopleCount(@PathVariable Short peopleCount){return null;}
+
+  @GetMapping("{city}")
+  public List<Object[]> findPainelsWithHighestAttentionByCity(@PathVariable String city){return null;}
   
-  public Attention findById(Long id){return null;}
-  List<EAttentionLevel> findByAttention(EAttentionLevel attention){return null;}
-  public List<Byte> findByAttentionLevel(Byte attention){return null;}
-  public List<Short> findByLookCount(Short lookCount){return null;}
-  public List<Short> findByPeopleCount(Short peopleCount){return null;}
-  public List<Object[]> findPainelsWithHighestAttentionByCity(String city){return null;}
-  public List<Painel> findPainelsWithAttentionAboveThreshold(Short threshold){return null;}
-  public List<Object[]> findPeakAttentionPeriodsByPainel(Long painelId){return null;}
-  public List<Object[]> findPainelsWithHighestAttentionByMonth(EMonth month){return null;}
-  public List<Object[]> findAverageAttentionByPainelInCity(String city){return null;}
-  public List<Object[]> findPainelsWithMostPeopleInPeriod(EPeriodOfTheDay period){return null;}
-  public Long findTotalLooksForPainelInWeek(Long painelId){return null;}
-  public List<Object[]> findInteractionCountByPainelAndMonth(EMonth month){return null;}
-  public List<Object[]> findAveragePeopleCountByCity(String city){return null;}
-  public List<Object[]> findPainelsWithMostInteractionsAtHour(Byte hour){return null;}
-  public List<Object[]> findMostViewedPainelsByDate(LocalDateTime date){return null;}
-  public List<Object[]> findHighestAttentionLevelByPainelInState(String state){return null;}
-  public List<Object[]> findAverageLooksByPainelAtHour(Byte hour){return null;}
-  public List<Object[]> findPainelsWithMostInteractionsByDayOfWeek(EDayWeek dayWeek){return null;}
+  @GetMapping("{threshold}")
+  public List<Painel> findPainelsWithAttentionAboveThreshold(@PathVariable Short threshold){return null;}
+
+  @GetMapping("/painel/{painel}")
+  public List<Object[]> findPeakAttentionPeriodsByPainel(@PathVariable Long painelId){return null;}
+
+  @GetMapping("{month}")
+  public List<Object[]> findPainelsWithHighestAttentionByMonth(@PathVariable EMonth month){return null;}
+
+  @GetMapping("/painel/{city}")
+  public List<Object[]> findAverageAttentionByPainelInCity(@PathVariable String city){return null;}
+
+  @GetMapping("{period}")
+  public List<Object[]> findPainelsWithMostPeopleInPeriod(@PathVariable EPeriodOfTheDay period){return null;}
+
+  @GetMapping("{painel-id}")
+  public Long findTotalLooksForPainelInWeek(@PathVariable Long painelId){return null;}
+
+  @GetMapping("/painel/{month}")
+  public List<Object[]> findInteractionCountByPainelAndMonth(@PathVariable EMonth month){return null;}
+
+  @GetMapping("{city-name}")
+  public List<Object[]> findAveragePeopleCountByCity(@PathVariable String city){return null;}
+
+  @GetMapping("{hour}")
+  public List<Object[]> findPainelsWithMostInteractionsAtHour(@PathVariable Byte hour){return null;}
+
+  @GetMapping("{date}")
+  public List<Object[]> findMostViewedPainelsByDate(@PathVariable LocalDateTime date){return null;}
+
+  @GetMapping("{state}")
+  public List<Object[]> findHighestAttentionLevelByPainelInState(@PathVariable String state){return null;}
+
+  @GetMapping("/painel/{hour}")
+  public List<Object[]> findAverageLooksByPainelAtHour(@PathVariable Byte hour){return null;}
+
+  @GetMapping("/painel/{day-week}")
+  public List<Object[]> findPainelsWithMostInteractionsByDayOfWeek(@PathVariable EDayWeek dayWeek){return null;}
 
 }
