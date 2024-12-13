7a96f80b5a60e30ec29f100afe1dcdec77f38326
matheusrebola <matheusrebola@outlook.com>
2024-12-13 11:07:41 -0300

    feat: configuração das rotas e das variáveis de caminho

--- service/analytics-layer/location-service/src/main/java/propagandus/locationservice/core/controllers/LocationController.java
+++ service/analytics-layer/location-service/src/main/java/propagandus/locationservice/core/controllers/LocationController.java
@@ -3,6 +3,7 @@ package propagandus.locationservice.core.controllers;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,26 +18,26 @@ public class LocationController {
   
 
   @GetMapping("{location-type}")
-  public List<ELocationType> findByLocationType(ELocationType locationType){return null;}
+  public List<ELocationType> findByLocationType(@PathVariable ELocationType locationType){return null;}
 
   @GetMapping("/reaction/{city}")
-  public List<Location> findLocationsByCityWithPositiveReactions(String city){return null;}
+  public List<Location> findLocationsByCityWithPositiveReactions(@PathVariable String city){return null;}
 
   @GetMapping("{country}")
-  public List<Location> findLocationsByCountryWithNegativeReactions(String country){return null;}
+  public List<Location> findLocationsByCountryWithNegativeReactions(@PathVariable String country){return null;}
 
   @GetMapping("{company}")
-  public List<Location> findLocationsWithAdvertisementsByCompany(String company){return null;}
+  public List<Location> findLocationsWithAdvertisementsByCompany(@PathVariable String company){return null;}
 
   @GetMapping("{city}")
-  public List<Object[]> countReactionsByTypeInCity(String city){return null;}
+  public List<Object[]> countReactionsByTypeInCity(@PathVariable String city){return null;}
 
   @GetMapping("{product}")
-  public List<Location> findLocationsWithReactionsByProduct(String product){return null;}
+  public List<Location> findLocationsWithReactionsByProduct(@PathVariable String product){return null;}
 
   @GetMapping("/advertising/{advertising}/reaction/{reaction}")
-  public List<Location> findLocationsWithMoreThanXReactionsForAdvertising(String advertisingName, Long reactionCount){return null;}
+  public List<Location> findLocationsWithMoreThanXReactionsForAdvertising(@PathVariable String advertisingName, @PathVariable Long reactionCount){return null;}
 
   @GetMapping("{state}")
-  public List<Location> findLocationsWithAdvertisementsAndReactionsByState(String state){return null;}
+  public List<Location> findLocationsWithAdvertisementsAndReactionsByState(@PathVariable String state){return null;}
 }
--- service/analytics-layer/painel-service/src/main/java/propagandus/painelservice/core/controllers/PainelController.java
+++ service/analytics-layer/painel-service/src/main/java/propagandus/painelservice/core/controllers/PainelController.java
@@ -2,6 +2,8 @@ package propagandus.painelservice.core.controllers;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,14 +16,33 @@ import propagandus.painelservice.core.models.enumerators.EStatus;
 @RequiredArgsConstructor
 public class PainelController {
 
-  String findByIdentification(String identification){return null;}
-  List<EStatus> findByEStatus(EStatus status){return null;}
-  List<String> findByModel(String model){return null;}
-  public List<Painel> findActivePainelsByCity(String city){return null;}
-  public List<Object[]> countInteractionsByPainelStatusInCity(String city){return null;}
-  public List<Painel> findByStatusAndCity(EStatus status, String city){return null;}
-  public List<Painel> findActivePainelsByModel(String model){return null;}
-  public List<Painel> findPainelsWithAttentionAboveLevel(Byte level){return null;}
-  public List<Object[]> countPainelsByModelInState(String state){return null;}
-  public List<Object[]> findMostInteractivePainelsByHour(Byte hour){return null;}
+  @GetMapping("{identification}")
+  String findByIdentification(@PathVariable String identification){return null;}
+
+  @GetMapping("{status}")
+  List<EStatus> findByEStatus(@PathVariable EStatus status){return null;}
+
+  @GetMapping("{model}")
+  List<String> findByModel(@PathVariable String model){return null;}
+
+  @GetMapping("/active/{city}")
+  public List<Painel> findActivePainelsByCity(@PathVariable String city){return null;}
+
+  @GetMapping("{city}")
+  public List<Object[]> countInteractionsByPainelStatusInCity(@PathVariable String city){return null;}
+
+  @GetMapping("/status/{status}/city/{city}")
+  public List<Painel> findByStatusAndCity(@PathVariable EStatus status, @PathVariable String city){return null;}
+
+  @GetMapping("/active/{model}")
+  public List<Painel> findActivePainelsByModel(@PathVariable String model){return null;}
+
+  @GetMapping("{level}")
+  public List<Painel> findPainelsWithAttentionAboveLevel(@PathVariable Byte level){return null;}
+
+  @GetMapping("{state}")
+  public List<Object[]> countPainelsByModelInState(@PathVariable String state){return null;}
+
+  @GetMapping("{hour}")
+  public List<Object[]> findMostInteractivePainelsByHour(@PathVariable Byte hour){return null;}
 }
