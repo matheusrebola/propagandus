f0b3f5faf68024a31b9fe27841d2b0fbe58c1a5f
matheusrebola <matheusrebola@outlook.com>
2024-12-13 11:39:15 -0300

    feat: criação de alguns métodos que pareciam ser interessantes + não atualização da repository

--- service/analytics-layer/advertising-service/src/main/java/propagandus/advertisingservice/core/controllers/AdvertisingController.java
+++ service/analytics-layer/advertising-service/src/main/java/propagandus/advertisingservice/core/controllers/AdvertisingController.java
@@ -1,47 +1,31 @@
 package propagandus.advertisingservice.core.controllers;
 
 import java.util.List;
-import java.util.stream.Collectors;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
-import propagandus.advertisingservice.core.dtos.AdvertisingDto;
-import propagandus.advertisingservice.core.mappers.AdvertisingMapper;
 import propagandus.advertisingservice.core.models.Advertising;
 import propagandus.advertisingservice.core.models.enumerators.EReactionType;
-import propagandus.advertisingservice.core.services.AdvertisingService;
 
 @RestController
 @RequestMapping("/advertising")
 @RequiredArgsConstructor
 public class AdvertisingController {
-  private final AdvertisingService advertisingService;
-  private final AdvertisingMapper advertisingMapper;
+  
+  @GetMapping("{name}")
+  List<String> findByName(String name){return null;}
+  
+  @GetMapping("{version}")
+  List<String> findByVersion(String version){return null;}
 
-  @GetMapping
-  public ResponseEntity<List<AdvertisingDto>> getAll(){
-    List<AdvertisingDto> result = advertisingService
-    .findAll()
-    .stream()
-    .map(advertisingMapper::map)
-    .collect(Collectors.toList());
-    return new ResponseEntity<>(result, HttpStatus.OK);
-  }
+  @GetMapping("{company}")
+  List<String> findByCompany(String company){return null;}
 
-  @GetMapping("{id}")
-  public ResponseEntity<AdvertisingDto> findById(@PathVariable Long id){
-    if(!advertisingService.exists(id)){
-      return ResponseEntity.notFound().build();
-    }
-    AdvertisingDto dto = advertisingMapper.map(advertisingService.findById(id));
-    return new ResponseEntity<>(dto, HttpStatus.OK);
-  }
+  @GetMapping("{product}")
+  List<String> findByProduct(String product){return null;}
 
   @GetMapping("/reaction/{company}")
   public List<Advertising> findPositiveReactionsByCompany(@PathVariable String company){return null;}
