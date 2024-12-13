8c15b70b43d8ad4f1e4fa29de877e1a2ae33567e
matheusrebola <matheusrebola@outlook.com>
2024-12-13 11:13:06 -0300

    feat: configuração das rotas e das variáveis de caminho

--- service/analytics-layer/period-register-service/src/main/java/propagandus/periodregisterservice/core/controllers/PeriodRegisterController.java
+++ service/analytics-layer/period-register-service/src/main/java/propagandus/periodregisterservice/core/controllers/PeriodRegisterController.java
@@ -3,6 +3,8 @@ package propagandus.periodregisterservice.core.controllers;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,10 +17,21 @@ import propagandus.periodregisterservice.core.models.enumerators.EMonth;
 @RequiredArgsConstructor
 public class PeriodRegisterController {
 
-  public List<PeriodRegister> findByTimeRange(LocalDateTime startTime, LocalDateTime endTime){return null;}
-  public List<PeriodRegister> findByMonthAndYear(EMonth month, String year){return null;}
-  public List<PeriodRegister> findByExactTime(Byte hour, Byte minute){return null;}
-  public List<PeriodRegister> findByMonthOrderedByTime(EMonth month){return null;}
-  public List<Object[]> findDayWithMostRegistersInMonth(EMonth month){return null;}
-  public List<Object[]> countRegistersBySpecificDays(List<Byte> days){return null;}
+  @GetMapping("/start/{start-time}/end/{end-time}")
+  public List<PeriodRegister> findByTimeRange(@PathVariable LocalDateTime startTime, @PathVariable LocalDateTime endTime){return null;}
+
+  @GetMapping("/month/{month}/year/{year}")
+  public List<PeriodRegister> findByMonthAndYear(@PathVariable EMonth month, @PathVariable String year){return null;}
+
+  @GetMapping("/hour/{hour}/minute/{minute}")
+  public List<PeriodRegister> findByExactTime(@PathVariable Byte hour, @PathVariable Byte minute){return null;}
+
+  @GetMapping("/period/{month}")
+  public List<PeriodRegister> findByMonthOrderedByTime(@PathVariable EMonth month){return null;}
+
+  @GetMapping("{month}")
+  public List<Object[]> findDayWithMostRegistersInMonth(@PathVariable EMonth month){return null;}
+
+  @GetMapping("{days}")
+  public List<Object[]> countRegistersBySpecificDays(@PathVariable List<Byte> days){return null;}
 }
