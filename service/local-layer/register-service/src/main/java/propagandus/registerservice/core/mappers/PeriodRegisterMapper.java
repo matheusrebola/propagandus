72f3f908ae38ecd91ea23f23f2a6303c11e64dc4
matheusrebola <matheusrebola@outlook.com>
2024-12-14 15:18:05 -0300

    feat: criação dos dois métodos responsáveis pela transformação de dto em entidade e de entidade em dto

--- service/local-layer/register-service/src/main/java/propagandus/registerservice/core/mappers/PeriodRegisterMapper.java
+++ service/local-layer/register-service/src/main/java/propagandus/registerservice/core/mappers/PeriodRegisterMapper.java
@@ -1,5 +1,24 @@
 package propagandus.registerservice.core.mappers;
 
-public class PeriodRegisterMapper {
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
 
+import lombok.RequiredArgsConstructor;
+import propagandus.registerservice.core.documents.PeriodRegister;
+import propagandus.registerservice.core.dtos.PeriodRegisterCreateDTO;
+import propagandus.registerservice.core.dtos.PeriodRegisterDTO;
+
+@Component
+@RequiredArgsConstructor
+public class PeriodRegisterMapper {
+  private final ModelMapper modelMapper;
+
+  public PeriodRegister map(PeriodRegisterCreateDTO dto){
+    PeriodRegister periodRegister = modelMapper.map(dto, PeriodRegister.class);
+    return periodRegister;
+  }
+  public PeriodRegisterDTO map(PeriodRegister periodRegister){
+    PeriodRegisterDTO dto = modelMapper.map(periodRegister, PeriodRegisterDTO.class);
+    return dto;
+  }
 }
