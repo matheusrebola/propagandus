4fd3249641114e7675f804bface6fb875719957d
matheusrebola <matheusrebola@outlook.com>
2024-12-14 10:53:31 -0300

    feat: remoção do DTO de criação + remoção do método que o utilizava na mapper

--- service/analytics-layer/advertising-service/src/main/java/propagandus/advertisingservice/core/mappers/AdvertisingMapper.java
+++ service/analytics-layer/advertising-service/src/main/java/propagandus/advertisingservice/core/mappers/AdvertisingMapper.java
@@ -4,7 +4,6 @@ import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
-import propagandus.advertisingservice.core.dtos.AdvertisingCreateDto;
 import propagandus.advertisingservice.core.dtos.AdvertisingDTO;
 import propagandus.advertisingservice.core.models.Advertising;
 
@@ -12,11 +11,6 @@ import propagandus.advertisingservice.core.models.Advertising;
 @RequiredArgsConstructor
 public class AdvertisingMapper {
   private final ModelMapper modelMapper;
-
-  public Advertising map(AdvertisingCreateDto dto){
-    Advertising advertising = modelMapper.map(dto, Advertising.class);
-    return advertising;
-  }
 
   public AdvertisingDTO map(Advertising advertising){
     AdvertisingDTO dto = modelMapper.map(advertising, AdvertisingDTO.class);
