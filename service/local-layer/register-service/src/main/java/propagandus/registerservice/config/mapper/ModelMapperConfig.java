4d016946221f636db59a99378e8af3bf410733f2
matheusrebola <matheusrebola@outlook.com>
2024-12-14 15:19:52 -0300

    feat: configuração da classe responsável por definir a model mapper

--- service/local-layer/register-service/src/main/java/propagandus/registerservice/config/mapper/ModelMapperConfig.java
+++ service/local-layer/register-service/src/main/java/propagandus/registerservice/config/mapper/ModelMapperConfig.java
@@ -1,5 +1,13 @@
 package propagandus.registerservice.config.mapper;
 
-public class ModelMapperConfig {
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+public class ModelMapperConfig {
+  @Bean
+  public ModelMapper modelMapper(){
+    return new ModelMapper();
+  }
 }
