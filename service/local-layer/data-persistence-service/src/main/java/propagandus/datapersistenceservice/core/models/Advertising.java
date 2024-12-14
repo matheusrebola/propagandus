f7bea3ea637c0a42a3462fcde5cf6eeb895d641a
matheusrebola <matheusrebola@outlook.com>
2024-12-14 15:28:26 -0300

    feat: atualização para persistência apenas de dados necessários a aplicação

--- service/local-layer/data-persistence-service/src/main/java/propagandus/datapersistenceservice/core/models/Advertising.java
+++ service/local-layer/data-persistence-service/src/main/java/propagandus/datapersistenceservice/core/models/Advertising.java
@@ -1,6 +1,7 @@
 package propagandus.datapersistenceservice.core.models;
 
 import java.util.List;
+import java.util.UUID;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
@@ -11,14 +12,12 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity(name = "advertising")
 @Table(name = "advertising_table")
 public record Advertising(
-  @Id @GeneratedValue(strategy = GenerationType.AUTO) @Column(name = "advertising_id") Long id,
-  @OneToOne @JoinColumn(name = "advertising_id", referencedColumnName = "advertising_info_id", nullable = false) AdvertisingInfo advertising,
+  @Id @GeneratedValue(strategy = GenerationType.UUID) @Column(name = "advertising_id") UUID id,
   @OneToMany(mappedBy = "advertising", cascade = CascadeType.ALL, orphanRemoval = true) List<Reaction> reactions,
   @OneToMany(mappedBy = "advertising", cascade = CascadeType.ALL, orphanRemoval = true) List<PeriodRegister> advertisingTime,
   @ManyToOne @JoinColumn(name = "painel_id", nullable = false) Painel painel
--- service/local-layer/data-persistence-service/src/main/java/propagandus/datapersistenceservice/core/models/PackageType.java
+++ service/local-layer/data-persistence-service/src/main/java/propagandus/datapersistenceservice/core/models/PackageType.java
@@ -1,11 +1,11 @@
 package propagandus.datapersistenceservice.core.models;
 
+import java.util.UUID;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
@@ -15,7 +15,7 @@ import propagandus.datapersistenceservice.core.models.enumerators.EPackageType;
 @Entity(name = "package_type")
 @Table(name = "package_type_table")
 public record PackageType(
-  @Id @GeneratedValue(strategy = GenerationType.AUTO) @Column(name = "package_type_id") Long id,
+  @Id @Column(name = "package_type_id") UUID id,
   @Enumerated(EnumType.STRING) @Column(length = 10) @NotNull EPackageType packageType,
   @OneToOne(mappedBy = "packageType") Painel painel
   ) {
