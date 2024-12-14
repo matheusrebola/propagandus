eacc1206bf64fd933a57c1423f99b1b13533ba8d
matheusrebola <matheusrebola@outlook.com>
2024-12-14 10:55:19 -0300

    feat: remoção de métodos desnecessários

--- service/cloud-layer/data-lake/src/main/java/propagandus/datalake/core/controllers/AdvertisingController.java
+++ service/cloud-layer/data-lake/src/main/java/propagandus/datalake/core/controllers/AdvertisingController.java
@@ -1,12 +1,7 @@
 package propagandus.datalake.core.controllers;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,25 +21,6 @@ import propagandus.datalake.core.services.AdvertisingService;
 public class AdvertisingController {
   private final AdvertisingService advertisingService;
   private final AdvertisingMapper advertisingMapper;
-
-  @GetMapping
-  public ResponseEntity<List<AdvertisingDto>> getAll(){
-    List<AdvertisingDto> result = advertisingService
-    .findAll()
-    .stream()
-    .map(advertisingMapper::map)
-    .collect(Collectors.toList());
-    return new ResponseEntity<>(result, HttpStatus.OK);
-  }
-
-  @GetMapping("{id}")
-  public ResponseEntity<AdvertisingDto> findById(@PathVariable Long id){
-    if(!advertisingService.exists(id)){
-      return ResponseEntity.notFound().build();
-    }
-    AdvertisingDto dto = advertisingMapper.map(advertisingService.findById(id));
-    return new ResponseEntity<>(dto, HttpStatus.OK);
-  }
 
   @PostMapping
   public ResponseEntity<AdvertisingDto> create(@Valid @RequestBody AdvertisingCreateDto requestDto){
