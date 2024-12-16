851196d14e7f06da2368df685a32296dc1153b5f
matheusrebola <matheusrebola@outlook.com>
2024-12-16 11:09:16 -0300

    feat: remoção de enumeradores desnecessários para a aplicação

--- service/local-layer/register-service/src/main/java/propagandus/registerservice/core/enumerators/EDayWeek.java
+++
@@ -1,5 +0,0 @@
-package propagandus.registerservice.core.enumerators;
-
-public enum EDayWeek {
-  MON, TUE, WED, THU, FRI, SAT, SUN
-}
--- service/local-layer/register-service/src/main/java/propagandus/registerservice/core/enumerators/EMonth.java
+++
@@ -1,5 +0,0 @@
-package propagandus.registerservice.core.enumerators;
-
-public enum EMonth {
-  JAN, FEB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC
-}
