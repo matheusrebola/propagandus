3f4529dd6ac7e85e27f99e87e0ce01d7e16f0096
matheusrebola <matheusrebola@outlook.com>
2024-12-18 20:29:10 -0300

    feat: criação dos enums

---
+++ service/cloud-layer/data-reciver-service/src/main/java/propagandus/datareciverservice/core/enumerators/EAttentionLevel.java
@@ -0,0 +1,5 @@
+package propagandus.datareciverservice.core.enumerators;
+
+public enum EAttentionLevel {
+  HIGH, MEDIUM, LOW, CRITICAL, URGENT, NORMAL, MINIMAL, SIGNIFICANT, IMPORTANT, ESSENTIAL
+}
