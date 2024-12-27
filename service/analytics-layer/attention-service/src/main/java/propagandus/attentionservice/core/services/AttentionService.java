2b2a502c4652e4d3f030d6113d49fc893114ff76
matheusrebola <matheusrebola@outlook.com>
2024-12-27 17:08:28 -0300

    feat: configuração da service para salvar as views no mongo

--- service/analytics-layer/attention-service/src/main/java/propagandus/attentionservice/core/services/AttentionService.java
+++ service/analytics-layer/attention-service/src/main/java/propagandus/attentionservice/core/services/AttentionService.java
@@ -5,15 +5,25 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+import propagandus.attentionservice.core.documents.AttentionByAudience;
 import propagandus.attentionservice.core.dtos.AttentionByAudienceDTO;
+import propagandus.attentionservice.core.mappers.AttentionByAudienceMapper;
+import propagandus.attentionservice.core.repositorys.AttentionByAudienceRepository;
 import propagandus.attentionservice.core.repositorys.AttentionRepository;
 
 @Service
 @RequiredArgsConstructor
 public class AttentionService {
   private final AttentionRepository attentionRepository;
+  private final AttentionByAudienceMapper attentionByAudienceMapper;
+  private final AttentionByAudienceRepository attentionByAudienceRepository;
 
-  public List<AttentionByAudienceDTO> attentionByAudience(){
-    return attentionRepository.attentionByAudience();
+  public List<AttentionByAudience> attentionByAudience(){
+    List<AttentionByAudienceDTO> dtoList = attentionRepository.attentionByAudience();
+    List<AttentionByAudience> attentions = dtoList.stream()
+      .map(attentionByAudienceMapper::map)
+      .toList();
+    List<AttentionByAudience> savedAttention = attentionByAudienceRepository.saveAll(attentions);
+    return savedAttention;
   }
 }
