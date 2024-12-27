aa41e9d9a77411395bbc30854b086cbbaea336d8
matheusrebola <matheusrebola@outlook.com>
2024-12-27 16:38:21 -0300

    feat: criação do microserviço responsável por chamar o orquestrator para que todas as views sejam criadas, em um esquema saga orquestrado

---
+++ service/analytics-layer/view-creation-service/src/main/java/propagandus/viewcreationservice/ViewCreationServiceApplication.java
@@ -0,0 +1,13 @@
+package propagandus.viewcreationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViewCreationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViewCreationServiceApplication.class, args);
+	}
+
+}
