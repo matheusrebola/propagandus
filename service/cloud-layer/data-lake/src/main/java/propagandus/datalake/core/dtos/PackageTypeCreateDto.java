04c42dfaa799d7bedae4272716e88706c0a6170b
matheusrebola <matheusrebola@outlook.com>
2024-12-27 18:42:17 -0300

    feat: remoção dos arquivos desnecessários ao funcionamento do serivço

--- service/cloud-layer/data-lake/src/main/java/propagandus/datalake/core/dtos/PackageTypeCreateDto.java
+++
@@ -1,9 +0,0 @@
-package propagandus.datalake.core.dtos;
-
-import propagandus.datalake.core.models.enumerators.EPackageType;
-
-public record PackageTypeCreateDto(
-  EPackageType packageType
-) {
-
-}
