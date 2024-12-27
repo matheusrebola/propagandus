8683cdd520fdb4a31ad69632ce355b5606965d35
matheusrebola <matheusrebola@outlook.com>
2024-12-27 17:09:18 -0300

    feat: configuração da service para salvar as views no mongo

--- service/analytics-layer/painel-service/src/main/java/propagandus/painelservice/core/services/PainelService.java
+++ service/analytics-layer/painel-service/src/main/java/propagandus/painelservice/core/services/PainelService.java
@@ -4,15 +4,25 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+import propagandus.painelservice.core.documents.PainelPerformance;
 import propagandus.painelservice.core.dtos.PainelPerformanceDTO;
+import propagandus.painelservice.core.mappers.PainelPerformanceMapper;
+import propagandus.painelservice.core.repositorys.PainelPerformanceRepository;
 import propagandus.painelservice.core.repositorys.PainelRepository;
 
 @Service
 @RequiredArgsConstructor
 public class PainelService {
   private final PainelRepository painelRepository;
-
-  List<PainelPerformanceDTO> painelPerformance(){
-    return painelRepository.painelPerformance();
+  private final PainelPerformanceMapper painelPerformanceMapper;
+  private final PainelPerformanceRepository painelPerformanceRepository;
+  
+  List<PainelPerformance> locationPerformance(){
+    List<PainelPerformanceDTO> dtoList = painelRepository.painelPerformance();
+    List<PainelPerformance> painels = dtoList.stream()
+      .map(painelPerformanceMapper::map)
+      .toList();
+    List<PainelPerformance> savedLocation = painelPerformanceRepository.saveAll(painels);
+    return savedLocation;
   }
 }
