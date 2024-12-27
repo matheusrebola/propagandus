f2fc67a59c397cd25b95cf3a559eaf19c9697951
matheusrebola <matheusrebola@outlook.com>
2024-12-27 13:46:27 -0300

    feat: configuração das services com seus métodos

--- service/analytics-layer/advertising-service/src/main/java/propagandus/advertisingservice/core/services/AdvertisingService.java
+++ service/analytics-layer/advertising-service/src/main/java/propagandus/advertisingservice/core/services/AdvertisingService.java
@@ -5,42 +5,40 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
-import propagandus.advertisingservice.core.dtos.AdvertisingCityReactionCountDTO;
-import propagandus.advertisingservice.core.dtos.AdvertisingDTO;
-import propagandus.advertisingservice.core.dtos.AdvertisingDetailsDTO;
-import propagandus.advertisingservice.core.dtos.AdvertisingReactionCountDTO;
-import propagandus.advertisingservice.core.dtos.AdvertisingReactionTypeCountDTO;
-import propagandus.advertisingservice.core.dtos.ProductReactionCountDTO;
-import propagandus.advertisingservice.core.models.Advertising;
-import propagandus.advertisingservice.core.models.enumerators.EReactionType;
+import propagandus.advertisingservice.core.documents.CampaignEffectiveness;
+import propagandus.advertisingservice.core.documents.ProductPreferences;
+import propagandus.advertisingservice.core.dtos.CampaignEffectivenessDTO;
+import propagandus.advertisingservice.core.dtos.ProductPreferencesDTO;
+import propagandus.advertisingservice.core.mappers.CampaignEffectivenessMapper;
+import propagandus.advertisingservice.core.mappers.ProductPreferencesMapper;
 import propagandus.advertisingservice.core.repositorys.AdvertisingRepository;
+import propagandus.advertisingservice.core.repositorys.CampaignEffectivenessRepository;
+import propagandus.advertisingservice.core.repositorys.ProductPreferencesRepository;
 
 @Service
 @RequiredArgsConstructor
 public class AdvertisingService {
   private final AdvertisingRepository advertisingRepository;
+  private final CampaignEffectivenessRepository campaignEffectivenessRepository;
+  private final ProductPreferencesRepository productPreferencesRepository;
+  private final CampaignEffectivenessMapper campaignEffectivenessMapper;
+  private final ProductPreferencesMapper productPreferencesMapper;
 
-  //repository
-  public Advertising findById(Long id){return advertisingRepository.findById(id).orElse(null);}
-  public boolean exists(Long id){return advertisingRepository.existsById(id);}
-  public List<String> findByName(String name){return advertisingRepository.findByName(name);}
-  public List<String> findByCompany(String company){return advertisingRepository.findByCompany(company);}
-  public List<String> findByProduct(String product){return advertisingRepository.findByProduct(product);}
+  public List<CampaignEffectiveness> campaignEffectiveness() {
+    List<CampaignEffectivenessDTO> dtoList = advertisingRepository.campaignEffectiveness();
+    List<CampaignEffectiveness> campaigns = dtoList.stream()
+      .map(campaignEffectivenessMapper::map)
+      .toList();
+    List<CampaignEffectiveness> savedCampaigns = campaignEffectivenessRepository.saveAll(campaigns);
+    return savedCampaigns;
+  }
 
-  //view
-  
-  //public List<AdvertisingDTO> findByReactionType(EReactionType reactionType){return advertisingRepository.findByReactionType(reactionType);}
-  //public List<AdvertisingDTO> findPositiveReactionsByCompany(String company){return advertisingRepository.findPositiveReactionsByCompany(company);}
-  //public List<AdvertisingDTO> findByCityWithReactions(String city){return findByCityWithReactions(city);}
-  //public List<AdvertisingDetailsDTO> findAdvertisingDetailsWithReactions(String name){return advertisingRepository.findAdvertisingDetailsWithReactions(name);}
-  //public List<AdvertisingDTO> findByStateWithReactions(String state){return advertisingRepository.findByStateWithReactions(state);}
-  //public List<AdvertisingReactionCountDTO> countReactionsByAdvertising(){return advertisingRepository.countReactionsByAdvertising();}
-  //public List<AdvertisingCityReactionCountDTO> findAdvertisingWithMostReactionsInCity(String city){return advertisingRepository.findAdvertisingWithMostReactionsInCity(city);}
-  //public List<AdvertisingReactionTypeCountDTO> findAdvertisingWithMostReactionsByType(EReactionType reactionType){return advertisingRepository.findAdvertisingWithMostReactionsByType(reactionType);}
-  //public List<ProductReactionCountDTO> countReactionsByProduct(){return advertisingRepository.countReactionsByProduct();}
-  //public List<AdvertisingDTO> findAdvertisingByPainel(Long painelId){return advertisingRepository.findAdvertisingByPainel(painelId);}
-  //public List<ProductReactionCountDTO> countReactionsByAdvertisingAndCity(){return advertisingRepository.countReactionsByAdvertisingAndCity();}
-  //public List<AdvertisingDTO> findAdvertisingByProductWithReactionsAboveThreshold(String product, Long threshold){return advertisingRepository.findAdvertisingByProductWithReactionsAboveThreshold(product, threshold);}
-  //public List<AdvertisingDTO> findAdvertisingInLocationsWithMorePainels(Byte painelCount){return advertisingRepository.findAdvertisingInLocationsWithMorePainels(painelCount);}
-  
+  public List<ProductPreferences> productPreferences(){
+    List<ProductPreferencesDTO> dtoList = advertisingRepository.productPreferences();
+    List<ProductPreferences> products = dtoList.stream()
+      .map(productPreferencesMapper::map)
+      .toList();
+    List<ProductPreferences> savedProducts = productPreferencesRepository.saveAll(products);
+    return savedProducts;
+  }   
 }
--- service/analytics-layer/advertising-service/src/main/java/propagandus/advertisingservice/core/services/mongo/AdvertisingService.java
+++
@@ -1,5 +0,0 @@
-package propagandus.advertisingservice.core.services.mongo;
-
-public class AdvertisingService {
-
-}
