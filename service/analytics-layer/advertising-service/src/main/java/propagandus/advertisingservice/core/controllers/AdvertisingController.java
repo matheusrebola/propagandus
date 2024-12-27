1497d6f4ee7016b1f99a21b7e917456cf8b0680f
matheusrebola <matheusrebola@outlook.com>
2024-12-27 13:47:40 -0300

    feat: configuração dos métodos que serão utilizados

--- service/analytics-layer/advertising-service/src/main/java/propagandus/advertisingservice/core/controllers/AdvertisingController.java
+++ service/analytics-layer/advertising-service/src/main/java/propagandus/advertisingservice/core/controllers/AdvertisingController.java
@@ -1,66 +1,33 @@
 package propagandus.advertisingservice.core.controllers;
 
 import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
-import propagandus.advertisingservice.core.dtos.ProductReactionCountDTO;
-import propagandus.advertisingservice.core.models.Advertising;
-import propagandus.advertisingservice.core.models.enumerators.EReactionType;
+import propagandus.advertisingservice.core.documents.CampaignEffectiveness;
+import propagandus.advertisingservice.core.documents.ProductPreferences;
+import propagandus.advertisingservice.core.services.AdvertisingService;
 
 @RestController
 @RequestMapping("/advertising")
 @RequiredArgsConstructor
 public class AdvertisingController {
+  private final AdvertisingService advertisingService;
 
-  /*@GetMapping("/reaction")
-  public ResponseEntity<List<AdvertisingReactionCountDTO>> countReactionsByAdvertising(){
-    List<AdvertisingReactionCountDTO> result = advertisingService.countReactionsByAdvertising().stream().collect(Collectors.toList());
+  @GetMapping("/campaign-effectiveness")
+  public ResponseEntity<List<CampaignEffectiveness>> campaignEffectiveness(){
+    List<CampaignEffectiveness> result = advertisingService.campaignEffectiveness();
     return new ResponseEntity<>(result, HttpStatus.OK);
-  }*/
+  }
 
-  @GetMapping("/by-product")
-  public List<ProductReactionCountDTO> countReactionsByProduct(){return null;}
-
-  @GetMapping("/by-city")
-  public List<ProductReactionCountDTO> countReactionsByAdvertisingAndCity(){return null;}
-  
-  @GetMapping("{name}")
-  List<String> findByName(@PathVariable String name){return null;}
-  
-  @GetMapping("{version}")
-  List<String> findByVersion(@PathVariable String version){return null;}
-
-  @GetMapping("{company}")
-  List<String> findByCompany(@PathVariable String company){return null;}
-
-  @GetMapping("{product}")
-  List<String> findByProduct(@PathVariable String product){return null;}
-
-  @GetMapping("/reaction/{company}")
-  public List<Advertising> findPositiveReactionsByCompany(@PathVariable String company){return null;}
-
-  @GetMapping("/reaction/{city}")
-  public List<Advertising> findByCityWithReactions(@PathVariable String city){return null;}
-
-  @GetMapping("/reaction/{name}")
-  public List<Object[]> findAdvertisingDetailsWithReactions(@PathVariable String name){return null;}
-
-  @GetMapping("/location/{state}")
-  public List<Advertising> findByStateWithReactions(@PathVariable String state){return null;}
-
-  @GetMapping("/location/{city}")
-  public List<Object[]> findAdvertisingWithMostReactionsInCity(@PathVariable String city){return null;}
-
-  @GetMapping("/reaction/{reaction-type}")
-  public List<Object[]> findAdvertisingWithMostReactionsByType(@PathVariable EReactionType reactionType){return null;}
-
-  @GetMapping("/painel/{painel-id}")
-  public List<Advertising> findAdvertisingByPainel(@PathVariable Long painelId){return null;}
-  
-  @GetMapping("/painel/{painel-count}")
-  public List<Advertising> findAdvertisingInLocationsWithMorePainels(@PathVariable Byte painelCount){return null;}
+  @GetMapping("/product-preferences")
+  public ResponseEntity<List<ProductPreferences>> productPreferences(){
+    List<ProductPreferences> result = advertisingService.productPreferences();
+    return new ResponseEntity<>(result, HttpStatus.OK);
+  }
 }
