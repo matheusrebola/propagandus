8fb3ad6d97b0f1d48f5502a146a88dc8a2b9443a
matheusrebola <matheusrebola@outlook.com>
2024-12-27 13:44:29 -0300

    feat: configuração das repositorys responsáveis por cada view

--- service/analytics-layer/advertising-service/src/main/java/propagandus/advertisingservice/core/repositorys/AdvertisingRepository.java
+++ service/analytics-layer/advertising-service/src/main/java/propagandus/advertisingservice/core/repositorys/AdvertisingRepository.java
@@ -1,17 +1,13 @@
 package propagandus.advertisingservice.core.repositorys;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import propagandus.advertisingservice.core.models.Advertising;
-import propagandus.advertisingservice.core.views.AdvertisingView;
+import propagandus.advertisingservice.core.views.CampaignEffectivenessView;
+import propagandus.advertisingservice.core.views.ProductPreferencesView;
 
 @Repository
-public interface AdvertisingRepository extends JpaRepository<Advertising,Long>,AdvertisingView {
-  List<String> findByName(String name);
-  List<String> findByVersion(String version);
-  List<String> findByCompany(String company);
-  List<String> findByProduct(String product); 
-}
+public interface AdvertisingRepository extends JpaRepository<Advertising,Long>
+  ,CampaignEffectivenessView
+  ,ProductPreferencesView {}
