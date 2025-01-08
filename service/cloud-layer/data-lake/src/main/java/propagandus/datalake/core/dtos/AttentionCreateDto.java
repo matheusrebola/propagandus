e85241d2d2180f62ee08f6fbc11f6ae99adbba58
Matheus Rebola <m.cavalcantir@outlook.com>
2025-01-08 19:30:53 -0300

    feat: criação manual dos getters, setters e contrutor

--- service/cloud-layer/data-lake/src/main/java/propagandus/datalake/core/dtos/AttentionCreateDto.java
+++ service/cloud-layer/data-lake/src/main/java/propagandus/datalake/core/dtos/AttentionCreateDto.java
@@ -3,16 +3,108 @@ package propagandus.datalake.core.dtos;
 import propagandus.datalake.core.models.enumerators.EAttentionLevel;
 import propagandus.datalake.core.models.enumerators.ESEx;
 
-public record AttentionCreateDto(
-  EAttentionLevel attentionLevel,
-  Byte attentionValue,
-  Short lookCount,
-  Short peopleCount,
-  ESEx sex,
-  Long attentionTime,
-  Long locationId,
-  Long advertisingId,
-  Long painelId
-) {
+public class AttentionCreateDto {
+	private EAttentionLevel attentionLevel;
+	private Byte attentionValue;
+	private Short lookCount;
+	private Short peopleCount;
+	private ESEx sex;
+	private Long periodId;
+	private Long locationId;
+	private Long advertisingId;
+	private Long painelId;
+	
+	public AttentionCreateDto(
+			EAttentionLevel attentionLevel,
+			Byte attentionValue,
+			Short lookCount,
+			Short peopleCount,
+			ESEx sex,
+			Long attentionTimeId,
+			Long locationId,
+			Long advertisingId,
+			Long painelId) {
+		super();
+		this.attentionLevel = attentionLevel;
+		this.attentionValue = attentionValue;
+		this.lookCount = lookCount;
+		this.peopleCount = peopleCount;
+		this.sex = sex;
+		this.periodId = attentionTimeId;
+		this.locationId = locationId;
+		this.advertisingId = advertisingId;
+		this.painelId = painelId;
+	}
 
+	public EAttentionLevel getAttentionLevel() {
+		return attentionLevel;
+	}
+
+	public void setAttentionLevel(EAttentionLevel attentionLevel) {
+		this.attentionLevel = attentionLevel;
+	}
+
+	public Byte getAttentionValue() {
+		return attentionValue;
+	}
+
+	public void setAttentionValue(Byte attentionValue) {
+		this.attentionValue = attentionValue;
+	}
+
+	public Short getLookCount() {
+		return lookCount;
+	}
+
+	public void setLookCount(Short lookCount) {
+		this.lookCount = lookCount;
+	}
+
+	public Short getPeopleCount() {
+		return peopleCount;
+	}
+
+	public void setPeopleCount(Short peopleCount) {
+		this.peopleCount = peopleCount;
+	}
+
+	public ESEx getSex() {
+		return sex;
+	}
+
+	public void setSex(ESEx sex) {
+		this.sex = sex;
+	}
+
+	public Long getPeriodId() {
+		return periodId;
+	}
+
+	public void setPeriodId(Long periodId) {
+		this.periodId = periodId;
+	}
+
+	public Long getLocationId() {
+		return locationId;
+	}
+
+	public void setLocationId(Long locationId) {
+		this.locationId = locationId;
+	}
+
+	public Long getAdvertisingId() {
+		return advertisingId;
+	}
+
+	public void setAdvertisingId(Long advertisingId) {
+		this.advertisingId = advertisingId;
+	}
+
+	public Long getPainelId() {
+		return painelId;
+	}
+
+	public void setPainelId(Long painelId) {
+		this.painelId = painelId;
+	}
 }
--- service/cloud-layer/data-lake/src/main/java/propagandus/datalake/core/dtos/PeriodRegisterCreateDto.java
+++ service/cloud-layer/data-lake/src/main/java/propagandus/datalake/core/dtos/PeriodRegisterCreateDto.java
@@ -1,17 +1,88 @@
 package propagandus.datalake.core.dtos;
 
-import propagandus.datalake.core.models.enumerators.EDayWeek;
-import propagandus.datalake.core.models.enumerators.EMonth;
+import java.time.DayOfWeek;
+import java.time.Month;
+
 import propagandus.datalake.core.models.enumerators.EPeriodOfTheDay;
 
-public record PeriodRegisterCreateDto(
-  Byte day,
-  EDayWeek dayWeek,
-  Byte hour,
-  Byte minute,
-  EPeriodOfTheDay periodOfTheDay,
-  EMonth month,
-  String year
-) {
+public class PeriodRegisterCreateDto {
+	private Byte day;
+	private DayOfWeek dayWeek;
+	private Byte hour;
+	private Byte minute;
+	private EPeriodOfTheDay periodOfTheDay;
+	private Month month;
+	private Short year;
+	
+	public PeriodRegisterCreateDto(Byte day, DayOfWeek dayWeek, Byte hour, Byte minute, EPeriodOfTheDay periodOfTheDay,
+			Month month, Short year) {
+		this.day = day;
+		this.dayWeek = dayWeek;
+		this.hour = hour;
+		this.minute = minute;
+		this.periodOfTheDay = periodOfTheDay;
+		this.month = month;
+		this.year = year;
+	}
 
+	public PeriodRegisterCreateDto() {
+		
+	}
+
+	public Byte getDay() {
+		return day;
+	}
+
+	public void setDay(Byte day) {
+		this.day = day;
+	}
+
+	public DayOfWeek getDayWeek() {
+		return dayWeek;
+	}
+
+	public void setDayWeek(DayOfWeek dayWeek) {
+		this.dayWeek = dayWeek;
+	}
+
+	public Byte getHour() {
+		return hour;
+	}
+
+	public void setHour(Byte hour) {
+		this.hour = hour;
+	}
+
+	public Byte getMinute() {
+		return minute;
+	}
+
+	public void setMinute(Byte minute) {
+		this.minute = minute;
+	}
+
+	public EPeriodOfTheDay getPeriodOfTheDay() {
+		return periodOfTheDay;
+	}
+
+	public void setPeriodOfTheDay(EPeriodOfTheDay periodOfTheDay) {
+		this.periodOfTheDay = periodOfTheDay;
+	}
+
+	public Month getMonth() {
+		return month;
+	}
+
+	public void setMonth(Month month) {
+		this.month = month;
+	}
+
+	public Short getYear() {
+		return year;
+	}
+
+	public void setYear(Short year) {
+		this.year = year;
+	}
 }
+  
