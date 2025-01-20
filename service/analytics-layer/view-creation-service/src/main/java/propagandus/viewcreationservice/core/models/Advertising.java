4e12e0e2173af2cf0031198670972ef74f016789
Matheus Rebola <m.cavalcantir@outlook.com>
2025-01-20 10:15:09 -0300

    feat: alteração do import para @Id

--- service/analytics-layer/view-creation-service/src/main/java/propagandus/viewcreationservice/core/models/Advertising.java
+++ service/analytics-layer/view-creation-service/src/main/java/propagandus/viewcreationservice/core/models/Advertising.java
@@ -3,13 +3,12 @@ package propagandus.viewcreationservice.core.models;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import org.springframework.data.annotation.Id;
-
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
