c0e79dc655b039008df0094ab4ad8c6ea1070d4e
Matheus Rebola <m.cavalcantir@outlook.com>
2025-06-11 13:31:10 -0300

    feat: configuração do redis para cache

--- service/persistence-service/src/main/java/propagandus/persistenceservice/PersistenceServiceApplication.java
+++ service/persistence-service/src/main/java/propagandus/persistenceservice/PersistenceServiceApplication.java
@@ -2,8 +2,10 @@ package propagandus.persistenceservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class PersistenceServiceApplication {
 
 	public static void main(String[] args) {
