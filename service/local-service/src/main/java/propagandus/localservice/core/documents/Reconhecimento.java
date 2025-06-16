92f8686b969e5aa0b6772e13dbce1644744b50c2
Matheus Rebola <m.cavalcantir@outlook.com>
2025-06-16 12:27:59 -0300

    feat: configuração da classe para utilização do sql

--- service/local-service/src/main/java/propagandus/localservice/core/documents/Reconhecimento.java
+++ service/local-service/src/main/java/propagandus/localservice/core/documents/Reconhecimento.java
@@ -1,23 +1,36 @@
 package propagandus.localservice.core.documents;
 
+import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Data;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 import propagandus.localservice.core.documents.enums.EAtencao;
 import propagandus.localservice.core.documents.enums.EIdade;
 import propagandus.localservice.core.documents.enums.ESexo;
 
 @Data
 @Builder
-@Document(collection = "reconhecimentos")
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "reconhecimento")
 public class Reconhecimento {
+
   @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
   private String id;
+
   private String data;
   private String local;
   private String token;
+
+  @Enumerated(EnumType.STRING)
   private ESexo sexo;
+
+  @Enumerated(EnumType.STRING)
   private EIdade idade;
+
+  @Enumerated(EnumType.STRING)
   private EAtencao atencao;
 }
--- service/local-service/src/main/java/propagandus/localservice/core/repositories/ReconhecimentoRepository.java
+++ service/local-service/src/main/java/propagandus/localservice/core/repositories/ReconhecimentoRepository.java
@@ -1,9 +1,9 @@
 package propagandus.localservice.core.repositories;
 
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import propagandus.localservice.core.documents.Reconhecimento;
 
 @Repository
-public interface ReconhecimentoRepository extends MongoRepository<Reconhecimento, String> {
+public interface ReconhecimentoRepository extends JpaRepository<Reconhecimento, String> {
 }
